46da91c626445bb8617f53a03bbd652c47ec274a
zflyluo <zflyluo@tencent.com>
2022-04-06 16:46:52 +0800

    重命名TDTheme的Map字段，防止歧义

--- lib/src/theme/td_fonts.dart
+++ lib/src/theme/td_fonts.dart
@@ -6,20 +6,20 @@ import 'basic.dart';
 extension TDFonts on TDThemeData {
   /// 字体大小/行高
   /// 36/44
-  Font? get fontXL => fonts['fontXL'];
+  Font? get fontXL => fontMap['fontXL'];
 
   /// 20/28
-  Font? get fontL => fonts['fontL'];
+  Font? get fontL => fontMap['fontL'];
 
   /// 16/24
-  Font? get fontM => fonts['fontM'];
+  Font? get fontM => fontMap['fontM'];
 
   /// 14/22
-  Font? get fontS => fonts['fontS'];
+  Font? get fontS => fontMap['fontS'];
 
   /// 12/20
-  Font? get fontXS => fonts['fontXS'];
+  Font? get fontXS => fontMap['fontXS'];
 
   /// 10/16
-  Font? get fontXXS => fonts['fontXXS'];
+  Font? get fontXXS => fontMap['fontXXS'];
 }
--- lib/src/theme/td_shadows.dart
+++ lib/src/theme/td_shadows.dart
@@ -4,11 +4,11 @@ import 'package:tdesign_flutter/src/theme/td_theme.dart';
 /// 内置投影
 extension TDBoxShadows on TDThemeData {
   /// 基础投影
-  List<BoxShadow>? get baseShadows => shadows['baseShadows'];
+  List<BoxShadow>? get baseShadows => shadowMap['baseShadows'];
 
   /// 中层投影
-  List<BoxShadow>? get middleShadows => shadows['middleShadows'];
+  List<BoxShadow>? get middleShadows => shadowMap['middleShadows'];
 
   /// 上层投影
-  List<BoxShadow>? get topShadows => shadows['topShadows'];
+  List<BoxShadow>? get topShadows => shadowMap['topShadows'];
 }
