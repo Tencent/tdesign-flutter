b43e64180c351b51d4cd1389c00cbd1a6e3eec7b
zflyluo <2515187986@qq.com>
2022-05-31 21:25:28 +0800

    分割线支持虚线分割

--- lib/src/components/divider/td_divider.dart
+++ lib/src/components/divider/td_divider.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:tdesign_flutter/src/components/divider/dashed_widget.dart';
 import 'package:tdesign_flutter/td_export.dart';
 
 /// 分割线
 /// 对于非flutter原有的控件，则只需满足TDesign规范即可；
 /// 如果有业务在实际使用，还需兼容实际业务场景。
 class TDDivider extends StatelessWidget {
-  const TDDivider({Key? key,
-  this.color,
+  const TDDivider({
+    Key? key,
+    this.color,
     this.margin,
     this.width,
     this.height,
@@ -15,6 +17,7 @@ class TDDivider extends StatelessWidget {
     this.gapPadding,
     this.hideLine = false,
     this.isDashed = false,
+    this.direction = Axis.horizontal,
   }) : super(key: key);
 
   /// 线条颜色
@@ -22,44 +25,41 @@ class TDDivider extends StatelessWidget {
 
   /// 外部填充
   final EdgeInsetsGeometry? margin;
-  
+
   /// 线条和中间文本之间的填充
   final EdgeInsetsGeometry? gapPadding;
 
   /// 宽度，需要竖向线条时使用
   final double? width;
-  
+
   /// 高度，横向线条使用
   final double? height;
-  
+
   /// 文本字符串，使用默认样式
   final String? text;
-  
+
   /// 中间控件，可自定义样式
   final Widget? widget;
-  
+
   /// 隐藏线条，使用纯文本分割
   final bool hideLine;
-  
-  /// 是否为虚线，TODO: 暂未实现
+
+  /// 是否为虚线
   final bool isDashed;
+
+  /// 方向,竖直虚线必须传
+  final Axis direction;
 
   @override
   Widget build(BuildContext context) {
     // 普通直线
-    if(widget == null && text == null) {
-      return Container(
-        width: width,
-        height: height ?? 0.5,
-        margin: margin,
-        color: color ?? TDTheme
-            .of(context)
-            .grayColor3,
-      );
+    if (widget == null && text == null) {
+      return _buildLine(context,
+          width: width, height: height, margin: margin, color: color);
     }
-    
+
     // 隐藏线条，纯文本分割
-    if(hideLine) {
+    if (hideLine) {
       return Container(
         width: width,
         height: height,
@@ -67,7 +67,7 @@ class TDDivider extends StatelessWidget {
         child: _buildMiddleWidget(context),
       );
     }
-    
+
     // 文本+线条
     return Container(
       width: width,
@@ -75,32 +75,64 @@ class TDDivider extends StatelessWidget {
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Expanded(child: Center(
-          child: Container(
+          Expanded(
+              child: Center(
+            child: _buildLine(
+              context,
+              height: height ?? 0.5,
+              color: color ?? TDTheme.of(context).grayColor3,
+            ),
+          )),
+          Padding(
+              padding: gapPadding ?? const EdgeInsets.only(left: 12, right: 12),
+              child: _buildMiddleWidget(context)),
+          Expanded(
+              child: Center(
+                  child: _buildLine(
+            context,
             height: height ?? 0.5,
-            color: color ?? TDTheme
-                .of(context)
-                .grayColor3,),
-      )),
-          Padding(padding: gapPadding ?? const EdgeInsets.only(left: 12, right: 12),
-          child: _buildMiddleWidget(context)),
-          Expanded(child: Center(
-    child: Container(
-            height: height ?? 0.5,
-            color: color ?? TDTheme
-              .of(context)
-              .grayColor3,))),
+            color: color ?? TDTheme.of(context).grayColor3,
+          ))),
         ],
       ),
     );
   }
 
+  /// 绘制线条
+  Container _buildLine(BuildContext context,
+      {double? width,
+      double? height,
+      EdgeInsetsGeometry? margin,
+      Color? color}) {
+    if (isDashed) {
+      return Container(
+        width: width,
+        margin: margin,
+        child: DashedWidget(
+          width: width,
+          height: height,
+          color: color ?? TDTheme.of(context).grayColor3,
+          direction: direction,
+        ),
+      );
+    } else {
+      return Container(
+        width: width,
+        height: height ?? 0.5,
+        margin: margin,
+        color: color ?? TDTheme.of(context).grayColor3,
+      );
+    }
+  }
+
   /// 构建中间控件
   _buildMiddleWidget(BuildContext context) {
-    return widget ?? TDText(text,
-      font: TDTheme.of(context).fontXS,
-      textColor: TDTheme.of(context).fontGyColor3,
-      forceVerticalCenter: true,
-    );
+    return widget ??
+        TDText(
+          text,
+          font: TDTheme.of(context).fontXS,
+          textColor: TDTheme.of(context).fontGyColor3,
+          forceVerticalCenter: true,
+        );
   }
 }
