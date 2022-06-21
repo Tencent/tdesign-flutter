8dfbdef8f4166d3eb7f49fdba720203793182f39
zflyluo <2515187986@qq.com>
2022-06-21 11:08:41 +0800

    合并code_convention代码，解决冲突

--- lib/src/theme/td_default_theme.dart
+++ lib/src/theme/td_default_theme.dart
@@ -2,215 +2,215 @@
 class TDDefaultTheme {
   static String defaultThemeConfig = '''
   {
-	'default': {
-		'color': {
-			'brandColor1': '#ECF2FE',
-			'brandColor2': '#D4E3FC',
-			'brandColor3': '#BBD3FB',
-			'brandColor4': '#96BBF8',
-			'brandColor5': '#699EF5',
-			'brandColor6': '#4787F0',
-			'brandColor7': '#266FE8',
-			'brandColor8': '#0052D9',
-			'brandColor9': '#0034B5',
-			'brandColor10': '#001F97',
-			'brandLightColor': '#ECF2FE',
-			'brandFocusColor': '#D4E3FC',
-			'brandDisabledColor': '#BBD3FB',
-			'brandHoverColor': '#266FE8',
-			'brandNormalColor': '#0052D9',
-			'brandClickColor': '#0034B5',
-			'errorColor1': '#FDECEE',
-			'errorColor2': '#F9D7D9',
-			'errorColor3': '#F8B9BE',
-			'errorColor4': '#F78D94',
-			'errorColor5': '#F36D78',
-			'errorColor6': '#E34D59',
-			'errorColor7': '#C9353F',
-			'errorColor8': '#B11F26',
-			'errorColor9': '#951114',
-			'errorColor10': '#680506',
-			'errorLightColor': '#FDECEE',
-			'errorFocusColor': '#F9D7D9',
-			'errorDisabledColor': '#F8B9BE',
-			'errorHoverColor': '#F36D78',
-			'errorNormalColor': '#E34D59',
-			'errorClickColor': '#C9353F',
-			'warningColor1': '#FEF3E6',
-			'warningColor2': '#F9E0C7',
-			'warningColor3': '#F7C797',
-			'warningColor4': '#F2995F',
-			'warningColor5': '#ED7B2F',
-			'warningColor6': '#D35A21',
-			'warningColor7': '#BA431B',
-			'warningColor8': '#9E3610',
-			'warningColor9': '#842B0B',
-			'warningColor10': '#5A1907',
-			'warningLightColor': '#FEF3E6',
-			'warningFocusColor': '#F9E0C7',
-			'warningDisabledColor': '#F7C797',
-			'warningHoverColor': '#F2995F',
-			'warningNormalColor': '#ED7B2F',
-			'warningClickColor': '#D35A21',
-			'successColor1': '#E8F8F2',
-			'successColor2': '#BCEBDC',
-			'successColor3': '#85DBBE',
-			'successColor4': '#48C79C',
-			'successColor5': '#00A870',
-			'successColor6': '#078D5C',
-			'successColor7': '#067945',
-			'successColor8': '#056334',
-			'successColor9': '#044F2A',
-			'successColor10': '#033017',
-			'successLightColor': '#E8F8F2',
-			'successFocusColor': '#BCEBDC',
-			'successDisabledColor': '#85DBBE',
-			'successHoverColor': '#48C79C',
-			'successNormalColor': '#00A870',
-			'successClickColor': '#078D5C',
-			'fontGyColor1': '#E6000000',
-			'fontGyColor2': '#99000000',
-			'fontGyColor3': '#66000000',
-			'fontGyColor4': '#42000000',
-			'fontWhColor1': '#FFFFFFFF',
-			'fontWhColor2': '#8CFFFFFF',
-			'fontWhColor3': '#59FFFFFF',
-			'fontWhColor4': '#38FFFFFF',
-			'whiteColor1': '#FFFFFF',
-			'grayColor1': '#F3F3F3',
-			'grayColor2': '#EEEEEE',
-			'grayColor3': '#E7E7E7',
-			'grayColor4': '#DCDCDC',
-			'grayColor5': '#C5C5C5',
-			'grayColor6': '#A6A6A6',
-			'grayColor7': '#8B8B8B',
-			'grayColor8': '#777777',
-			'grayColor9': '#5E5E5E',
-			'grayColor10': '#4B4B4B',
-			'grayColor11': '#383838',
-			'grayColor12': '#2C2C2C',
-			'grayColor13': '#242424',
-			'grayColor14': '#181818'
+	"default": {
+		"color": {
+			"brandColor1": "#ECF2FE",
+			"brandColor2": "#D4E3FC",
+			"brandColor3": "#BBD3FB",
+			"brandColor4": "#96BBF8",
+			"brandColor5": "#699EF5",
+			"brandColor6": "#4787F0",
+			"brandColor7": "#266FE8",
+			"brandColor8": "#0052D9",
+			"brandColor9": "#0034B5",
+			"brandColor10": "#001F97",
+			"brandLightColor": "#ECF2FE",
+			"brandFocusColor": "#D4E3FC",
+			"brandDisabledColor": "#BBD3FB",
+			"brandHoverColor": "#266FE8",
+			"brandNormalColor": "#0052D9",
+			"brandClickColor": "#0034B5",
+			"errorColor1": "#FDECEE",
+			"errorColor2": "#F9D7D9",
+			"errorColor3": "#F8B9BE",
+			"errorColor4": "#F78D94",
+			"errorColor5": "#F36D78",
+			"errorColor6": "#E34D59",
+			"errorColor7": "#C9353F",
+			"errorColor8": "#B11F26",
+			"errorColor9": "#951114",
+			"errorColor10": "#680506",
+			"errorLightColor": "#FDECEE",
+			"errorFocusColor": "#F9D7D9",
+			"errorDisabledColor": "#F8B9BE",
+			"errorHoverColor": "#F36D78",
+			"errorNormalColor": "#E34D59",
+			"errorClickColor": "#C9353F",
+			"warningColor1": "#FEF3E6",
+			"warningColor2": "#F9E0C7",
+			"warningColor3": "#F7C797",
+			"warningColor4": "#F2995F",
+			"warningColor5": "#ED7B2F",
+			"warningColor6": "#D35A21",
+			"warningColor7": "#BA431B",
+			"warningColor8": "#9E3610",
+			"warningColor9": "#842B0B",
+			"warningColor10": "#5A1907",
+			"warningLightColor": "#FEF3E6",
+			"warningFocusColor": "#F9E0C7",
+			"warningDisabledColor": "#F7C797",
+			"warningHoverColor": "#F2995F",
+			"warningNormalColor": "#ED7B2F",
+			"warningClickColor": "#D35A21",
+			"successColor1": "#E8F8F2",
+			"successColor2": "#BCEBDC",
+			"successColor3": "#85DBBE",
+			"successColor4": "#48C79C",
+			"successColor5": "#00A870",
+			"successColor6": "#078D5C",
+			"successColor7": "#067945",
+			"successColor8": "#056334",
+			"successColor9": "#044F2A",
+			"successColor10": "#033017",
+			"successLightColor": "#E8F8F2",
+			"successFocusColor": "#BCEBDC",
+			"successDisabledColor": "#85DBBE",
+			"successHoverColor": "#48C79C",
+			"successNormalColor": "#00A870",
+			"successClickColor": "#078D5C",
+			"fontGyColor1": "#E6000000",
+			"fontGyColor2": "#99000000",
+			"fontGyColor3": "#66000000",
+			"fontGyColor4": "#42000000",
+			"fontWhColor1": "#FFFFFFFF",
+			"fontWhColor2": "#8CFFFFFF",
+			"fontWhColor3": "#59FFFFFF",
+			"fontWhColor4": "#38FFFFFF",
+			"whiteColor1": "#FFFFFF",
+			"grayColor1": "#F3F3F3",
+			"grayColor2": "#EEEEEE",
+			"grayColor3": "#E7E7E7",
+			"grayColor4": "#DCDCDC",
+			"grayColor5": "#C5C5C5",
+			"grayColor6": "#A6A6A6",
+			"grayColor7": "#8B8B8B",
+			"grayColor8": "#777777",
+			"grayColor9": "#5E5E5E",
+			"grayColor10": "#4B4B4B",
+			"grayColor11": "#383838",
+			"grayColor12": "#2C2C2C",
+			"grayColor13": "#242424",
+			"grayColor14": "#181818"
 		},
-		'font': {
-			'fontXL': {
-				'size': 36,
-				'lineHeight': 44
+		"font": {
+			"fontXL": {
+				"size": 36,
+				"lineHeight": 44
 			},
-			'fontL': {
-				'size': 20,
-				'lineHeight': 28
+			"fontL": {
+				"size": 20,
+				"lineHeight": 28
 			},
-			'fontM': {
-				'size': 16,
-				'lineHeight': 24
+			"fontM": {
+				"size": 16,
+				"lineHeight": 24
 			},
-			'fontS': {
-				'size': 14,
-				'lineHeight': 22
+			"fontS": {
+				"size": 14,
+				"lineHeight": 22
 			},
-			'fontXS': {
-				'size': 12,
-				'lineHeight': 20
+			"fontXS": {
+				"size": 12,
+				"lineHeight": 20
 			},
-			'fontXXS': {
-				'size': 10,
-				'lineHeight': 16
+			"fontXXS": {
+				"size": 10,
+				"lineHeight": 16
 			}
 		},
-		'fontFamily': {
-			'numberFontFamily': {
-				'fontFamily': 'DINAlternate-B'
+		"fontFamily": {
+			"numberFontFamily": {
+				"fontFamily": "DINAlternate-B"
 			}
 		},
-		'corner': {
-			'baseBorderRadius': 8
+		"corner": {
+			"baseBorderRadius": 8
 		},
-		'shadow': {
-			'baseShadows': [
+		"shadow": {
+			"baseShadows": [
 			    {
-			      'color': '#0D000000',
-			      'blurRadius': 10,
-			      'spreadRadius': 1,
-			      'offset':{
-			        'x':0,
-			        'y':1
+			      "color": "#0D000000",
+			      "blurRadius": 10,
+			      "spreadRadius": 1,
+			      "offset":{
+			        "x":0,
+			        "y":1
 			       }
 			    },
 			    {
-			      'color': '#14000000',
-			      'blurRadius': 5,
-			      'spreadRadius': 1,
-			      'offset':{
-			        'x':0,
-			        'y':4
+			      "color": "#14000000",
+			      "blurRadius": 5,
+			      "spreadRadius": 1,
+			      "offset":{
+			        "x":0,
+			        "y":4
 			       }
 			    },
 			    {
-			      'color': '#1F000000',
-			      'blurRadius': 4,
-			      'spreadRadius': -1,
-			      'offset':{
-			        'x':0,
-			        'y':2
+			      "color": "#1F000000",
+			      "blurRadius": 4,
+			      "spreadRadius": -1,
+			      "offset":{
+			        "x":0,
+			        "y":2
 			       }
 			    }
 			  ],
-			'middleShadows': [
+			"middleShadows": [
 			    {
-			      'color': '#0D000000',
-			      'blurRadius': 14,
-			      'spreadRadius': 2,
-			      'offset':{
-			        'x':0,
-			        'y':3
+			      "color": "#0D000000",
+			      "blurRadius": 14,
+			      "spreadRadius": 2,
+			      "offset":{
+			        "x":0,
+			        "y":3
 			       }
 			    },
 			    {
-			      'color': '#0F000000',
-			      'blurRadius': 10,
-			      'spreadRadius': 1,
-			      'offset':{
-			        'x':0,
-			        'y':8
+			      "color": "#0F000000",
+			      "blurRadius": 10,
+			      "spreadRadius": 1,
+			      "offset":{
+			        "x":0,
+			        "y":8
 			       }
 			    },
 			    {
-			      'color': '#1A000000',
-			      'blurRadius': 5,
-			      'spreadRadius': -3,
-			      'offset':{
-			        'x':0,
-			        'y':0
+			      "color": "#1A000000",
+			      "blurRadius": 5,
+			      "spreadRadius": -3,
+			      "offset":{
+			        "x":0,
+			        "y":0
 			       }
 			    }
 			  ],
-			'topShadows': [
+			"topShadows": [
 			    {
-			      'color': '#0D000000',
-			      'blurRadius': 30,
-			      'spreadRadius': 5,
-			      'offset':{
-			        'x':0,
-			        'y':6
+			      "color": "#0D000000",
+			      "blurRadius": 30,
+			      "spreadRadius": 5,
+			      "offset":{
+			        "x":0,
+			        "y":6
 			       }
 			    },
 			    {
-			      'color': '#0A000000',
-			      'blurRadius': 24,
-			      'spreadRadius': 2,
-			      'offset':{
-			        'x':0,
-			        'y':16
+			      "color": "#0A000000",
+			      "blurRadius": 24,
+			      "spreadRadius": 2,
+			      "offset":{
+			        "x":0,
+			        "y":16
 			       }
 			    },
 			    {
-			      'color': '#14000000',
-			      'blurRadius': 10,
-			      'spreadRadius': -5,
-			      'offset':{
-			        'x':0,
-			        'y':8
+			      "color": "#14000000",
+			      "blurRadius": 10,
+			      "spreadRadius": -5,
+			      "offset":{
+			        "x":0,
+			        "y":8
 			       }
 			    }
 			  ]
