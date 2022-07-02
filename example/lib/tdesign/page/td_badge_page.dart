701176727cda7d7be28b104f140e1c667f2410b2
jackjflin <jackjflin@tencent.com>
2022-07-02 22:48:44 +0800

    徽标组件完善

--- example/lib/tdesign/page/td_badge_page.dart
+++ example/lib/tdesign/page/td_badge_page.dart
@@ -14,28 +14,80 @@ class _TdBadgePageState extends State<TdBadgePage> {
     return ExampleWidget(
       title: 'Badge组件',
       children: [
-
-        TDBadge(
-          TDBadgeType.redPoint,
-        ),
-        TDBadge(
-          TDBadgeType.remind,
-        ),
-        TDBadge(
-          TDBadgeType.message,
-          count: 2,
-        ),
-        TDBadge(TDBadgeType.message, count: 16),
-        TDBadge(TDBadgeType.message, count: 162),
-        TDBadge(
-          TDBadgeType.message,
-          isNew: true,
-        ),
-        TDBadge(
-          TDBadgeType.message,
-          isNew: true,
-          border: TDBadgeBorder.small,
-        ),
+        ExampleItem(
+            desc: '红点徽标',
+            builder: (_) {
+              return const TDBadge(
+                TDBadgeType.redPoint,
+              );
+            }),
+        ExampleItem(
+            desc: '提醒徽标',
+            builder: (_) {
+              return const TDBadge(
+                TDBadgeType.remind,
+              );
+            }),
+        ExampleItem(
+            desc: '消息徽标-个位数',
+            builder: (_) {
+              return const TDBadge(
+                TDBadgeType.message,
+                count: 2,
+              );
+            }),
+        ExampleItem(
+            desc: '消息徽标-两位数',
+            builder: (_) {
+              return const TDBadge(TDBadgeType.message, count: 16);
+            }),
+        ExampleItem(
+            desc: '消息徽标-三位数',
+            builder: (_) {
+              return const TDBadge(TDBadgeType.message, count: 128);
+            }),
+        ExampleItem(
+            desc: '消息徽标-自定义内容',
+            builder: (_) {
+              return const TDBadge(
+                TDBadgeType.message,
+                message: '新消息提醒',
+              );
+            }),
+        ExampleItem(
+            desc: '消息徽标-自定义内容-方形',
+            builder: (_) {
+              return const TDBadge(
+                TDBadgeType.message,
+                message: 'New',
+                border: TDBadgeBorder.small,
+              );
+            }),
+        ExampleItem(
+            desc: '消息徽标-自定义内容-角标',
+            builder: (_) {
+              return Stack(
+                alignment: Alignment.topRight,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(left: 16),
+                    alignment: Alignment.centerLeft,
+                    child: TDText(
+                      '单行标题',
+                      textColor: TDTheme.of(context).fontGyColor1,
+                      font: TDTheme.of(context).fontM,
+                    ),
+                    color: Colors.white,
+                    height: 48,
+                    width: MediaQuery.of(context).size.width,
+                  ),
+                  const TDBadge(
+                    TDBadgeType.subscript,
+                    message: 'NEW',
+                  ),
+                ],
+              );
+            }),
       ],
     );
   }
