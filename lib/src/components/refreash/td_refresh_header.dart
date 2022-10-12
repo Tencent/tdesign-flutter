84f85f005326b997426c004c3f9e7521b952a824
zflyluo <2515187986@qq.com>
2022-10-12 16:48:05 +0800

    增加下拉刷新组件example

---
+++ lib/src/components/refreash/td_refresh_header.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_easyrefresh/easy_refresh.dart';
+
+import '../../../td_export.dart';
+
+/// TDesign刷新头部
+class TDRefreshHeader extends Header {
+  /// Key
+  final Key? key;
+
+  TDRefreshHeader({
+    this.key,
+    double extent = 48.0,
+    double triggerDistance = 48.0,
+    bool float = false,
+    Duration? completeDuration,
+    bool enableHapticFeedback = true,
+    bool enableInfiniteRefresh = false,
+    bool overScroll = true,
+  }) : super(
+          extent: extent,
+          triggerDistance: triggerDistance,
+          float: float,
+          completeDuration: completeDuration,
+          enableHapticFeedback: enableHapticFeedback,
+          enableInfiniteRefresh: enableInfiniteRefresh,
+          overScroll: overScroll,
+        );
+
+  @override
+  Widget contentBuilder(
+      BuildContext context,
+      RefreshMode refreshState,
+      double pulledExtent,
+      double refreshTriggerPullDistance,
+      double refreshIndicatorExtent,
+      AxisDirection axisDirection,
+      bool float,
+      Duration? completeDuration,
+      bool enableInfiniteRefresh,
+      bool success,
+      bool noMore) {
+    // 不能为水平方向
+    assert(
+        axisDirection == AxisDirection.down ||
+            axisDirection == AxisDirection.up,
+        'Widget cannot be horizontal');
+
+    return TDLoadingWidget(
+      key: key,
+      size: TDLoadingSize.medium,
+      icon: TDLoadingIcon.circle,
+      iconColor: TDTheme.of(context).brandColor8,
+    );
+  }
+}
