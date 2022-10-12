d4479fcf44e9fb7f0be4689433e39708231e1c8a
jackjflin <jackjflin@tencent.com>
2022-10-12 10:02:44 +0800

    补充横向选项卡样例

--- example/lib/tdesign/page/td_tab_bar_page.dart
+++ example/lib/tdesign/page/td_tab_bar_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tdesign_flutter/td_export.dart';
+import '../example_base.dart';
 
 // ignore: use_key_in_widget_constructors
 class TdTabBarPage extends StatefulWidget {
@@ -10,7 +11,13 @@ class TdTabBarPage extends StatefulWidget {
 class _TdTabBarPageState extends State<TdTabBarPage>
     with TickerProviderStateMixin {
   TabController? _tabController;
+  TabController? _tabController1;
+  TabController? _tabController2;
+  TabController? _tabController3;
+  TabController? _tabController4;
+  TabController? _tabController5;
   List<TDTab> tabs = [];
+  List<Widget> tabViews = [];
 
   List<TDTab> _getTabs() {
     tabs = const [
@@ -18,64 +25,128 @@ class _TdTabBarPageState extends State<TdTabBarPage>
       TDTab(text: '标签页二'),
       TDTab(text: '标签页三'),
       TDTab(text: '标签页四'),
+      TDTab(text: '标签页五'),
     ];
     return tabs;
   }
 
   List<Widget> _getTabViews() {
-    return [
-      ListView.builder(
-        itemBuilder: (context, index) => const Text('123'),
-        itemCount: 50,
-      ),
-      ListView.builder(
-        itemBuilder: (context, index) => const Text('123'),
-        itemCount: 50,
-      ),
-      ListView.builder(
-        itemBuilder: (context, index) => const Text('123'),
-        itemCount: 50,
-      ),
-      ListView.builder(
-        itemBuilder: (context, index) => const Text('123'),
-        itemCount: 50,
-      ),
+    tabViews = const [
+      Center(child: TDText('内容一')),
+      Center(child: TDText('内容二')),
+      Center(child: TDText('内容三')),
+      Center(child: TDText('内容四')),
     ];
+    return tabViews;
+  }
+
+  @override
+  initState() {
+    _initTabController();
+    _getTabs();
+    super.initState();
+  }
+
+  List<TDTab> subList(int length) {
+    var temp = <TDTab>[];
+    for(var i = 0; i < length; i++) {
+      temp.add(tabs[i]);
+    }
+    return temp;
   }
 
   //初始化tab
-  void _initTabController(BuildContext context) {
-    var tabNum = _getTabs().length;
-    _tabController = TabController(length: tabNum, vsync: this);
+  void _initTabController() {
+    _tabController1 = TabController(length: 2, vsync: this);
+    _tabController2 = TabController(length: 3, vsync: this);
+    _tabController3 = TabController(length: 4, vsync: this);
+    _tabController4 = TabController(length: 5, vsync: this);
+    _tabController5 = TabController(length: 4, vsync: this);
+    _tabController = TabController(length: 5, vsync: this);
   }
 
   @override
   Widget build(BuildContext context) {
-    if (_tabController == null) {
-      _initTabController(context);
-    }
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tabbar组件'),
-      ),
-      body: NestedScrollView(
-        headerSliverBuilder: (context, innerBoxIsScrolled) {
-          return [
-            SliverToBoxAdapter(
-                child: TDTabBar(
+    return ExampleWidget(
+      title: 'Tabs组件',
+      padding: const EdgeInsets.symmetric(vertical: 16),
+      children: [
+        ExampleItem(
+          desc: '横向选项卡',
+          builder: (BuildContext context) {
+            return Column(
+              children: [
+                TDTabBar(
+                  tabs: subList(2),
+                  controller: _tabController1,
+                  backgroundColor: Colors.white,
+                  showIndicator: true,
+                ),
+                TDTabBar(
+                  tabs: subList(3),
+                  controller: _tabController2,
+                  backgroundColor: Colors.white,
+                  showIndicator: true,
+                ),
+                TDTabBar(
+                  tabs: subList(4),
+                  controller: _tabController3,
+                  backgroundColor: Colors.white,
+                  showIndicator: true,
+                ),
+                TDTabBar(
+                  tabs: _getTabs(),
+                  controller: _tabController4,
+                  backgroundColor: Colors.white,
+                  showIndicator: true,
+                  isScrollable: true,
+                ),
+              ],
+            );
+          },
+        ),
+        ExampleItem(
+          desc: '无下划线横向选项卡',
+          builder: (BuildContext context) {
+            return TDTabBar(
               tabs: _getTabs(),
               controller: _tabController,
               backgroundColor: Colors.white,
-              showIndicator: true,
-            )),
-          ];
-        },
-        body: TDTabBarView(
-          isSlideSwitch: true,
-          controller: _tabController,
-          children: _getTabViews(),
+              isScrollable: true,
+            );
+          },
         ),
-      ),
+        ExampleItem(
+          desc: '竖向选项卡',
+          builder: (BuildContext context) {
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              height: 4 * 54,
+              child: Row(
+                children: [
+                  TDTabBar(
+                      width: 104,
+                      tabs: subList(4),
+                      controller: _tabController5,
+                      showIndicator: true,
+                      backgroundColor: Colors.white,
+                      isScrollable: false,
+                      isVertical: true
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width - 104,
+                    color: Colors.white,
+                    child: TDTabBarVerticalView(
+                      children: _getTabViews(),
+                      controller: _tabController5,
+                    ),
+                  )
+                ],
+              ),
+            );
+          },
+        ),
+      ],
     );
   }
 }
