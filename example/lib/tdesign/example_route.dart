4923aaf2011f6500d0cb94572e062aff96682a56
zflyluo <zflyluo@tencent.com>
2022-10-17 11:10:18 +0800

    添加关于页面

--- example/lib/tdesign/example_route.dart
+++ example/lib/tdesign/example_route.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:tdesign_flutter_example/about.dart';
 import '../main.dart';
 
 
 class TDExampleRoute {
   static final Map<String, Function> _routes = {};
+  static const String aboutPath = 'about';
 
   static void init(){
     for(var model in examplePageList){
       _routes[model.path] = model.pageBuilder;
     }
+    // 添加关于页路由
+    _routes[aboutPath] = (context)=> const AboutPage();
   }
 
   static Route<dynamic>? onGenerateRoute(RouteSettings settings) {
