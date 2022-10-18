9be2f95d118e2ecd02942544d0e5a0d3bae6f6e4
zflyluo <2515187986@qq.com>
2022-10-18 16:49:58 +0800

    增加代码拷贝功能

--- example/lib/tdesign/code_widget.dart
+++ example/lib/tdesign/code_widget.dart
@@ -2,8 +2,12 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:http/http.dart' as http;
+import 'package:markdown/markdown.dart' as md;
 import 'package:tdesign_flutter/td_export.dart';
+
+import 'syntax_highlighter.dart';
 
 class CodeWidget extends StatefulWidget {
   const CodeWidget({Key? key, this.codePath}) : super(key: key);
@@ -15,10 +19,8 @@ class CodeWidget extends StatefulWidget {
 }
 
 class _CodeWidgetState extends State<CodeWidget> {
-
   String? result;
   String? lastApiName;
-
 
   @override
   void initState() {
@@ -28,14 +30,17 @@ class _CodeWidgetState extends State<CodeWidget> {
 
   Future<String> getCodeData() async {
     const defaultResult = '加载失败';
-    if(widget.codePath == lastApiName && result != null && result != defaultResult){
+    if (widget.codePath == lastApiName &&
+        result != null &&
+        result != defaultResult) {
       return result!;
     }
     try {
-      var url = 'https://raw.githubusercontent.com/TDesignOteam/tdesign-flutter/main/example/lib/tdesign/page/td_${widget.codePath}_page.dart';
+      var url =
+          'https://raw.githubusercontent.com/TDesignOteam/tdesign-flutter/main/example/lib/tdesign/page/td_${widget.codePath}_page.dart';
 
       print('getCodeData url:$url');
-      var resp =  await http.get(Uri.parse(url));
+      var resp = await http.get(Uri.parse(url));
       result = String.fromCharCodes(resp.body.codeUnits);
       lastApiName = widget.codePath;
     } catch (e) {
@@ -47,21 +52,60 @@ class _CodeWidgetState extends State<CodeWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: FutureBuilder(
-        future: getCodeData(),
-        builder: (context, AsyncSnapshot<String> snapshot) {
-          if (snapshot.connectionState == ConnectionState.done) {
-            return SingleChildScrollView(
-              child: SelectableText(snapshot.data ?? '',),
-            );
-          } else {
-            return  Container(
-              alignment: Alignment.topLeft,
-              child: const TDText('加载中…'),
-            );
-          }
-        },
-      )
-    );
+        alignment: Alignment.topLeft,
+        child: FutureBuilder(
+          future: getCodeData(),
+          builder: (context, AsyncSnapshot<String> snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return Column(
+                children: [
+                  Container(
+                    height: 36,
+                    color: TDTheme.of(context).brandColor1,
+                    child: Row(
+                      children: [
+                        GestureDetector(
+                          child: const Icon(TDIcons.file_copy),
+                          onTap: () {
+                            if (result != null) {
+                              Clipboard.setData(ClipboardData(text: result!));
+                              TDToast.showText('复制成功', context: context);
+                            } else {
+                              TDToast.showText('复制失败', context: context);
+                            }
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                      child: Markdown(
+                    padding: EdgeInsets.zero,
+                    selectable: true,
+                    shrinkWrap: true,
+                    syntaxHighlighter: DartSyntaxHighlighter(),
+                    data: '''
+```dart
+${snapshot.data}
+```
+                  ''',
+                    extensionSet: md.ExtensionSet(
+                      md.ExtensionSet.gitHubWeb.blockSyntaxes,
+                      [
+                        md.EmojiSyntax(),
+                        ...md.ExtensionSet.gitHubWeb.inlineSyntaxes
+                      ],
+                    ),
+                  ))
+                ],
+              );
+            } else {
+              return Container(
+                alignment: Alignment.topLeft,
+                child: const TDText('加载中…'),
+              );
+            }
+          },
+        ));
   }
 }
