bd5edae5b400d297f599379a73df159d6257b0b1
zflyluo <zflyluo@tencent.com>
2022-12-06 21:56:30 +0800

    完成演示代码的展示

--- example/transform/lib/hook/node_utils.dart
+++ example/transform/lib/hook/node_utils.dart
@@ -2,29 +2,31 @@ import 'package:kernel/ast.dart';
 
 class NodeUtils {
   static const String kAnnotationClass = 'Demo';
-  static String kImportUri = 'package:tdesign_flutter_example/annotation/demo.dart';
+  static String kImportUri =
+      'package:tdesign_flutter_example/annotation/demo.dart';
 
   /// @Demo 目标类
-  static bool checkIfClassEnable(List<Expression> annotations) {
+  static ConstructorInvocation checkIfClassEnable(List<Expression> annotations) {
     if (annotations == null) {
-      return false;
+      return null;
     }
 
-    var enabled = false;
     for (var annotation in annotations) {
       //注解有ConstantExpression和ConstructorInvocation两种形式
       if (annotation is ConstantExpression) {
-        final constantExpression = annotation;
-        final constant = constantExpression.constant;
-        if (constant is InstanceConstant) {
-          final instanceConstant = constant;
-          final Class instanceClass = instanceConstant.classReference.node;
-          //@Demo注解
-          if (instanceClass.name == kAnnotationClass && kImportUri == (instanceClass?.parent as Library)?.importUri.toString()) {
-            enabled = true;
-            break;
-          }
-        }
+        break;
+        // final constantExpression = annotation;
+        // final constant = constantExpression.constant;
+        // if (constant is InstanceConstant) {
+        //   final instanceConstant = constant;
+        //   final Class instanceClass = instanceConstant.classReference.node;
+        //   //@Demo注解
+        //   if (instanceClass.name == kAnnotationClass && kImportUri == (instanceClass?.parent as Library)?.importUri.toString()) {
+        //     enabled = true;
+        //     return instanceClass;
+        //     break;
+        //   }
+        // }
       } else if (annotation is ConstructorInvocation) {
         final constructorInvocation = annotation;
         final Class cls = constructorInvocation.targetReference.node?.parent;
@@ -32,12 +34,12 @@ class NodeUtils {
           continue;
         }
         final Library library = cls?.parent;
-        if (cls.name == kAnnotationClass && kImportUri == library.importUri.toString()) {
-          enabled = true;
-          break;
+        if (cls.name == kAnnotationClass &&
+            kImportUri == library.importUri.toString()) {
+          return annotation;
         }
       }
     }
-    return enabled;
+    return null;
   }
 }
