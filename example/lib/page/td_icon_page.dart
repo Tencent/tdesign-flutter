9837b64d0b3c65a12056c26b13a461cebe19bd48
zflyluo <2515187986@qq.com>
2023-01-30 10:19:48 +0800

    添加两种icon图标的展示

--- example/lib/page/td_icon_page.dart
+++ example/lib/page/td_icon_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tdesign_flutter/td_export.dart';
+import 'package:tdesign_flutter/src/components/icon/td_icons_new.dart' as codesign;
 
 import '../../base/example_widget.dart';
 
@@ -16,7 +17,7 @@ class _TDIconPageState extends State<TDIconPage> {
   Widget build(BuildContext context) {
     return ExamplePage(title: 'icon图标',
         children: [
-        ExampleModule(title: '默认',
+        ExampleModule(title: 'figma',
         children: [
           ExampleItem(
               desc: 'icon数量: ${TDIcons.all.length}',
@@ -39,7 +40,32 @@ class _TDIconPageState extends State<TDIconPage> {
           ],
         ),
       ))
-    ])]);
+    ]),
+        ExampleModule(title: 'codesign',
+        children: [
+          ExampleItem(
+              desc: 'icon数量: ${TDIcons.all.length}',
+              builder: (_) => Container(
+        color: Colors.white,
+        alignment: Alignment.center,
+        child: Wrap(
+          children: [
+            for (var iconData in codesign.TDIcons.all.values) SizedBox(
+              height: 100,
+              width: 175,
+
+              child: Column(
+                children: [
+                  Icon(iconData),
+                  TDText(iconData.name)
+                ],
+              ),
+            )
+          ],
+        ),
+      ))
+    ]),
+        ]);
 
   }
 }
