763e37c91b305c4f02dd5560dcb64789710bc622
simonlei <simonlei@tencent.com>
2023-04-12 16:00:06 +0800

    can display links in all kinds state/style/size

--- lib/src/components/link/td_link.dart
+++ lib/src/components/link/td_link.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:tdesign_flutter/td_export.dart';
+
+import '../../../td_export.dart';
 
 enum TDLinkType {
   basic,
@@ -32,34 +33,75 @@ class TDLink extends StatelessWidget {
   const TDLink({
     Key? key,
     required this.label,
-    required this.uri,
     this.prefixIcon,
     this.suffixIcon,
+    this.followLink,
     this.type = TDLinkType.basic,
     this.style = TDLinkStyle.defaultStyle,
     this.state = TDLinkState.normal,
     this.size = TDLinkSize.medium,
+    this.color,
+    this.iconSize,
+    this.fontSize,
+    this.leftGapWithIcon,
+    this.rightGapWithIcon,
   }) : super(key: key);
 
+  /// link label to display
   final String label;
-  final Uri uri;
+
+  /// link type
   final TDLinkType type;
+
+  /// link style
   final TDLinkStyle style;
+
+  /// link state
   final TDLinkState state;
+
+  /// link size
   final TDLinkSize size;
+
+  /// prefix icon
   final Icon? prefixIcon;
+
+  /// suffix icon
   final Icon? suffixIcon;
+
+  /// link label color, calculated by state and style if not set
+  final Color? color;
+
+  /// link icon size, calculated by state and style if not set
+  final double? iconSize;
+
+  /// link font size, calculated by state and style if not set
+  final double? fontSize;
+
+  /// gap between icon and text, calculated by state and style if not set
+  final double? leftGapWithIcon;
+
+  /// gap between icon and text, calculated by state and style if not set
+  final double? rightGapWithIcon;
+
+  /// action when the link clicked
+  final Function? followLink;
 
   @override
   Widget build(BuildContext context) {
     if (type == TDLinkType.withPrefixIcon) {
       return Row(children: [
         prefixIcon == null ? _getDefaultIcon(context) : prefixIcon!,
+        SizedBox(
+          width: _getLeftGapSize(context),
+        ),
         _buildLink(context),
       ]);
     } else if (type == TDLinkType.withSuffixIcon) {
       return Row(children: [
         _buildLink(context),
+        SizedBox(
+          width: _getRightGapSize(context),
+        ),
         suffixIcon == null ? _getDefaultIcon(context) : suffixIcon!,
       ]);
     }
@@ -68,40 +110,83 @@ class TDLink extends StatelessWidget {
   }
 
   Color getColor(BuildContext context) {
-    switch (style) {
-      case TDLinkStyle.primary:
-        return TDTheme.of(context).brandNormalColor;
-      case TDLinkStyle.danger:
-        return TDTheme.of(context).errorColor6;
-      case TDLinkStyle.warning:
-        return TDTheme.of(context).warningColor5;
-      case TDLinkStyle.success:
-        return TDTheme.of(context).successColor5;
+    if (color != null) {
+      return color!;
     }
-    return TDTheme.of(context).fontGyColor1;
+    // to refactor: use map instead of multi level switch
+    switch (state) {
+      case TDLinkState.normal:
+        switch (style) {
+          case TDLinkStyle.primary:
+            return TDTheme.of(context).brandNormalColor;
+          case TDLinkStyle.danger:
+            return TDTheme.of(context).errorColor6;
+          case TDLinkStyle.warning:
+            return TDTheme.of(context).warningColor5;
+          case TDLinkStyle.success:
+            return TDTheme.of(context).successColor5;
+        }
+        return TDTheme.of(context).fontGyColor1;
+
+      case TDLinkState.active:
+        switch (style) {
+          case TDLinkStyle.primary:
+            return TDTheme.of(context).brandClickColor;
+          case TDLinkStyle.danger:
+            return TDTheme.of(context).errorColor7;
+          case TDLinkStyle.warning:
+            return TDTheme.of(context).warningColor6;
+          case TDLinkStyle.success:
+            return TDTheme.of(context).successColor6;
+        }
+        return TDTheme.of(context).brandClickColor;
+      case TDLinkState.disabled:
+        switch (style) {
+          case TDLinkStyle.primary:
+            return TDTheme.of(context).brandDisabledColor;
+          case TDLinkStyle.danger:
+            return TDTheme.of(context).errorDisabledColor;
+          case TDLinkStyle.warning:
+            return TDTheme.of(context).warningDisabledColor;
+          case TDLinkStyle.success:
+            return TDTheme.of(context).successDisabledColor;
+        }
+    }
+    return TDTheme.of(context).fontGyColor4;
   }
 
   Icon _getDefaultIcon(BuildContext context) {
     return Icon(
       TDIcons.link,
-      size: _getSize(context),
+      size: _getIconSize(context),
       color: getColor(context),
     );
   }
 
-  Text _buildLink(BuildContext context) {
-    return Text(
-      label,
-      style: TextStyle(
-        fontSize: _getFontSize(context),
-        color: getColor(context),
-        decoration:
-            type == TDLinkType.withUnderline ? TextDecoration.underline : null,
-      ),
-    );
+  _buildLink(BuildContext context) {
+    return InkWell(
+        onTap: () {
+          if (state == TDLinkState.disabled || followLink == null) {
+            return;
+          }
+          followLink!();
+        },
+        child: Text(
+          label,
+          style: TextStyle(
+            fontSize: _getFontSize(context),
+            color: getColor(context),
+            decoration: type == TDLinkType.withUnderline
+                ? TextDecoration.underline
+                : null,
+          ),
+        ));
   }
 
-  double _getSize(BuildContext context) {
+  double _getIconSize(BuildContext context) {
+    if (iconSize != null) {
+      return iconSize!;
+    }
     switch (size) {
       case TDLinkSize.large:
         return 11.99;
@@ -112,6 +197,9 @@ class TDLink extends StatelessWidget {
   }
 
   double _getFontSize(BuildContext context) {
+    if (fontSize != null) {
+      return fontSize!;
+    }
     switch (size) {
       case TDLinkSize.large:
         return 16;
@@ -119,5 +207,31 @@ class TDLink extends StatelessWidget {
         return 12;
     }
     return 14;
+  }
+
+  _getLeftGapSize(BuildContext context) {
+    if (leftGapWithIcon != null) {
+      return leftGapWithIcon!;
+    }
+    switch (size) {
+      case TDLinkSize.large:
+        return 14.64;
+      case TDLinkSize.small:
+        return 6.05;
+    }
+    return 6.34;
+  }
+
+  _getRightGapSize(BuildContext context) {
+    if (rightGapWithIcon != null) {
+      return rightGapWithIcon!;
+    }
+    switch (size) {
+      case TDLinkSize.large:
+        return 15.37;
+      case TDLinkSize.small:
+        return 6.63;
+    }
+    return 7;
   }
 }
