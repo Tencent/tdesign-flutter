b9f7d907ee4136c96716ea60ce7b7b7e1b5ae221
zflyluo <zflyluo@tencent.com>
2023-06-27 21:56:21 +0800

    适配singleChile类型的web_md生成逻辑

--- example/lib/base/web_md_tool.dart
+++ example/lib/base/web_md_tool.dart
@@ -8,11 +8,10 @@ import 'example_base.dart';
 import 'example_widget.dart';
 
 class WebMdTool {
-
   WebMdTool._();
 
   /// 用于生成web端的md,正常使用不要开启
-  static const bool needGenerateWebMd = true;
+  static const bool needGenerateWebMd = false;
 
   /// 生成web端的md
   static void generateWebMd({
@@ -21,69 +20,19 @@ class WebMdTool {
     required String? exampleCodeGroup,
     required List<ExampleModule> exampleModuleList,
     required List<ExampleItem> testList,
-}) async {
-    if(needGenerateWebMd && model != null && !kIsWeb){
-
+    required CodeWrapper? singleChild,
+  }) async {
+    if (needGenerateWebMd && model != null && !kIsWeb) {
       var exampleCodeSb = StringBuffer();
       var count = 1;
-      for(var module in exampleModuleList) {
-        exampleCodeSb.writeln('### $count ${module.title}');
-        for(var item in module.children){
-
-          exampleCodeSb.writeln('');
-          exampleCodeSb.writeln('${item.desc}');
-
-          var hasCodeSuccess = false;
-          if(!item.ignoreCode) {
-            var assetsPath = _getCodeAssetsPath(item, exampleCodeGroup ?? '');
-            if (assetsPath.isNotEmpty) {
-              try {
-                var codeString = await rootBundle.loadString(assetsPath);
-                if (codeString.isNotEmpty) {
-                  exampleCodeSb.writeln('''
-                  
-<td-code-block panel="Dart">
-
-  <pre slot="Dart" lang="javascript">${codeString}</pre>
-
-</td-code-block>
-                                        ''');
-                  hasCodeSuccess = true;
-                }
-              } catch (e) {
-                print(e);
-              }
-            }
-          } else {
-            var list = manualExampleCode[getItemKey(exampleCodeGroup, module.title, item.desc)];
-            if(list != null && list.isNotEmpty) {
-              list.forEach((element) {
-                exampleCodeSb.writeln('');
-                exampleCodeSb.writeln('''
-                
-<td-code-block panel="Dart">
-
-  <pre slot="Dart" lang="javascript">${element}</pre>
-
-</td-code-block>
-                      ''');
-              });
-              hasCodeSuccess = true;
-            } else {
-              print('error item:${exampleCodeGroup}_${module.title}_${item.desc},已忽略代码，请手动处理');
-            }
+      if (singleChild != null) {
+        await writeSingleCode(exampleCodeSb, exampleCodeGroup);
+      } else {
+        for (var module in exampleModuleList) {
+          exampleCodeSb.writeln('### $count ${module.title}');
+          for (var item in module.children) {
+            await writeCode(exampleCodeSb, item, exampleCodeGroup, module);
           }
-          if(!hasCodeSuccess){
-            exampleCodeSb.writeln('''
-            
-<td-code-block panel="Dart">
-
-  <pre slot="Dart" lang="javascript">暂无演示代码</pre>
-
-</td-code-block>
-                      ''');
-          }
-
         }
       }
 
@@ -93,16 +42,110 @@ class WebMdTool {
 暂无对应api
 ''';
       try {
-        api =  await rootBundle.loadString('assets/api/${model.name}_api.md');
+        api = await rootBundle.loadString('assets/api/${model.name}_api.md');
       } catch (e) {
         print(e);
       }
-      var mdContent = _getTemplate(model.text, description, model.spline ?? 'other', exampleCodeSb.toString(), api.toString());
+      var mdContent = _getTemplate(model.text, description,
+          model.spline ?? 'other', exampleCodeSb.toString(), api.toString());
       var file = File('/sdcard/td/web_md/${model.name}/README.md');
-      if(!file.existsSync()){
+      if (!file.existsSync()) {
         file.createSync(recursive: true);
       }
       file.writeAsStringSync(mdContent);
+    }
+  }
+
+  static Future<void> writeSingleCode(
+      StringBuffer exampleCodeSb, String? exampleCodeGroup) async {
+    var hasCodeSuccess = false;
+
+    var list = manualExampleCode[exampleCodeGroup];
+    if (list != null && list.isNotEmpty) {
+      list.forEach((element) {
+        exampleCodeSb.writeln('');
+        exampleCodeSb.writeln('''
+          
+<td-code-block panel="Dart">
+
+  <pre slot="Dart" lang="javascript">${element}</pre>
+
+</td-code-block>
+                ''');
+      });
+      hasCodeSuccess = true;
+    } else {
+      print('error item:singleChild_${exampleCodeGroup},已忽略代码，请手动处理');
+    }
+
+    if (!hasCodeSuccess) {
+      exampleCodeSb.writeln('''
+      
+<td-code-block panel="Dart">
+
+  <pre slot="Dart" lang="javascript">暂无演示代码</pre>
+
+</td-code-block>
+                ''');
+    }
+  }
+
+  static Future<void> writeCode(StringBuffer exampleCodeSb, ExampleItem item,
+      String? exampleCodeGroup, ExampleModule module) async {
+    exampleCodeSb.writeln('');
+    exampleCodeSb.writeln('${item.desc}');
+
+    var hasCodeSuccess = false;
+    if (!item.ignoreCode) {
+      var assetsPath = _getCodeAssetsPath(item, exampleCodeGroup ?? '');
+      if (assetsPath.isNotEmpty) {
+        try {
+          var codeString = await rootBundle.loadString(assetsPath);
+          if (codeString.isNotEmpty) {
+            exampleCodeSb.writeln('''
+            
+<td-code-block panel="Dart">
+
+  <pre slot="Dart" lang="javascript">${codeString}</pre>
+
+</td-code-block>
+                                  ''');
+            hasCodeSuccess = true;
+          }
+        } catch (e) {
+          print(e);
+        }
+      }
+    } else {
+      var list = manualExampleCode[
+          getItemKey(exampleCodeGroup, module.title, item.desc)];
+      if (list != null && list.isNotEmpty) {
+        list.forEach((element) {
+          exampleCodeSb.writeln('');
+          exampleCodeSb.writeln('''
+          
+<td-code-block panel="Dart">
+
+  <pre slot="Dart" lang="javascript">${element}</pre>
+
+</td-code-block>
+                ''');
+        });
+        hasCodeSuccess = true;
+      } else {
+        print(
+            'error item:${exampleCodeGroup}_${module.title}_${item.desc},已忽略代码，请手动处理');
+      }
+    }
+    if (!hasCodeSuccess) {
+      exampleCodeSb.writeln('''
+      
+<td-code-block panel="Dart">
+
+  <pre slot="Dart" lang="javascript">暂无演示代码</pre>
+
+</td-code-block>
+                ''');
     }
   }
 
@@ -128,8 +171,8 @@ class WebMdTool {
     return '';
   }
 
-  static String getSpline(String key){
-    switch(key){
+  static String getSpline(String key) {
+    switch (key) {
       case '基础':
         return 'base';
       case '导航':
@@ -145,17 +188,18 @@ class WebMdTool {
     }
   }
 
-  static String getItemKey(exampleCodeGroup,moduleTitle,itemDesc){
+  static String getItemKey(exampleCodeGroup, moduleTitle, itemDesc) {
     return '${exampleCodeGroup}_${moduleTitle}_${itemDesc}';
   }
 
-  static  _getTemplate(
+  static _getTemplate(
     String title,
     String description,
     String spline,
     String exampleCode,
     String api,
-      )=> '''
+  ) =>
+      '''
 ---
 title: $title
 description: ${description}
@@ -180,5 +224,5 @@ ${api}
 
   ''';
 
-  static var manualExampleCode = <String,List<String>>{};
+  static var manualExampleCode = <String, List<String>>{};
 }
