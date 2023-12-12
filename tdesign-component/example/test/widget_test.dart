c9a88d88243a0f04f7351d2eb86695036f65ba37
Luozf12345 <2515187986@qq.com>
2023-12-12 14:11:26 +0800

    添加渲染测试

--- tdesign-component/example/test/widget_test.dart
+++ tdesign-component/example/test/widget_test.dart
@@ -7,24 +7,80 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:tdesign_flutter/tdesign_flutter.dart';
 
 import 'package:tdesign_flutter_example/main_run.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Counter increments smoke testeee', (WidgetTester tester) async {
+    examplePageList.forEach((element) async {
+      testWidgets('Counter increments smoke teseeet', (WidgetTester tester) async {
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+        await _testComponent(tester, element.name);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+      });
+    });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
   });
+
+
+}
+
+Future<void> _testComponent(WidgetTester tester, String name) async {
+  print('当前组件：$name');
+  // Build our app and trigger a frame.
+  await tester.pumpWidget(const MyApp());
+
+  var button = find.text(name);
+
+  expect(button, findsOneWidget);
+  await tester.tap(button);
+
+  // try {
+  //   await tester.pumpAndSettle(
+  //           const Duration(milliseconds: 100),
+  //         EnginePhase.sendSemanticsUpdate,
+  //         const Duration(minutes: 100),
+  //       );
+  // } catch (e) {
+  //   print(e);
+  // }
+  await tester.pump();
+  await tester.pump();
+  await tester.pump();
+  var page = find.text('WebGenTag');
+  expect(page, findsOneWidget);
+
+  await tester.pump();
+
+  await tester.fling(page, const Offset(0, -20000), 2);
+  await tester.pumpAndSettle();
+  var genBtn = find.text('生成Web使用md');
+  expect(genBtn, findsOneWidget);
+  // // await _flingUtilFind(genBtn,page, tester);
+  await tester.tap(genBtn);
+}
+
+Future<void> _genMd(String name) async {
+
+}
+
+Future<void> _flingUtilFind(Finder genBtn, Finder page, WidgetTester tester) async {
+  // await tester.scrollUntilVisible(genBtn, 100);
+  var offset =100.0;
+  var sum = offset;
+  var found = false;
+  while(sum < 5000 && !found){
+    found = true;
+    sum += offset;
+    await tester.fling(page, Offset(0, -offset), 2);
+    await tester.pump(const Duration(milliseconds: 50));
+    try {
+      expect(genBtn, findsOneWidget);
+    } catch (e) {
+      found = false;
+      print('下滑距离：$sum, 未找到Widget，继续下滑');
+    }
+  }
+  return ;
 }
