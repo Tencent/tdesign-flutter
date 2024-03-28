73825ffa62ea58bfeabbb1a4164011b98041721e
Luozf12345 <2515187986@qq.com>
2024-03-28 21:38:40 +0800

    修改主题实现方式,支持ref定义

--- tdesign-component/example/lib/page/td_font_page.dart
+++ tdesign-component/example/lib/page/td_font_page.dart
@@ -11,43 +11,66 @@ class TDFontPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // debugPaintBaselinesEnabled = true;
-    return ExamplePage(
-        padding: const EdgeInsets.all(8),
-        title: tdTitle(context),
-        exampleCodeGroup: 'fonts',
-        children: [
-          ExampleModule(title: 'Token', children: [
-            ExampleItem(
-                ignoreCode: true,
-                builder: (context) {
-                  var children = <Widget>[];
-                  TDTheme.of(context).fontMap.forEach((key, value) {
-                    children.add(Container(
-                      child: TDText(
-                        '@$key:${value.size.toInt()}px',
-                        font: value,
+    return ExamplePage(padding: const EdgeInsets.all(8), title: tdTitle(context), exampleCodeGroup: 'fonts', children: [
+      ExampleModule(title: 'Token', children: [
+        ExampleItem(
+            ignoreCode: true,
+            builder: (context) {
+              var children = <Widget>[];
+              TDTheme.of(context).fontMap.forEach((key, value) {
+                children.add(Container(
+                  child: TDText(
+                    '@$key:${value.size.toInt()}px',
+                    font: value,
 
-                        /// link类型的示例添加下划线
-                        style: TextStyle(
-                            decoration: key.contains('Link')
-                                ? TextDecoration.underline
-                                : null,
-                            decorationColor: TDTheme.of(context).fontGyColor1),
-                      ),
-                      decoration: BoxDecoration(
-                          border: Border(
-                              bottom: BorderSide(
-                                  color: TDTheme.of(context).grayColor4,
-                                  width: 0.5))),
-                    ));
-                  });
-                  return ListView(
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    children: children,
-                  );
-                })
-          ]),
-        ]);
+                    /// link类型的示例添加下划线
+                    style: TextStyle(
+                        decoration: key.contains('Link') ? TextDecoration.underline : null,
+                        decorationColor: TDTheme.of(context).fontGyColor1),
+                  ),
+                  decoration: BoxDecoration(
+                      border: Border(bottom: BorderSide(color: TDTheme.of(context).grayColor4, width: 0.5))),
+                ));
+              });
+              return ListView(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                children: children,
+              );
+            })
+      ]),
+    ], test: [
+      ExampleItem(
+          ignoreCode: true,
+          builder: (context) {
+            return Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: TDText('使用主题字体:fontBodySmall', font: TDTheme.of(context).fontBodySmall),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: TDText('使用主题字体:fontBodyLarge', font: TDTheme.of(context).fontBodyLarge),
+                ),
+                Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: TDText(
+                      '不使用数字字体:1234567890abcd',
+                      font: TDTheme.defaultData().fontTitleSmall,
+                      textColor: TDTheme.of(context).brandColor6,
+                    )),
+                Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: TDText(
+                      '使用数字字体:1234567890abcd',
+                      font: TDTheme.defaultData().fontTitleSmall,
+                      textColor: TDTheme.of(context).brandColor6,
+                      fontFamily: TDTheme.defaultData().numberFontFamily,
+                    )),
+              ],
+            );
+          })
+    ]);
   }
 }
