305c6bccd2328e289e28b641f7b9b715666c9916
hkaikai <617760820@qq.com>
2024-04-10 20:14:14 +0800

    dropdown弹框完成

--- tdesign-component/lib/src/components/dropdown_menu/td_dropdown_item.dart
+++ tdesign-component/lib/src/components/dropdown_menu/td_dropdown_item.dart
@@ -5,7 +5,6 @@ class TDDropdownItem<T> extends StatefulWidget {
   const TDDropdownItem({
     Key? key,
     this.disabled = false,
-    this.keys,
     this.label,
     this.multiple = false,
     this.options = const [],
@@ -15,58 +14,77 @@ class TDDropdownItem<T> extends StatefulWidget {
     this.onConfirm,
     this.onReset,
   }) : super(key: key);
+
   /// 是否禁用
   final bool? disabled;
-  /// 用来定义 value / label 在 options 中对应的字段别名
-  final TDDropdownItemKeys? keys;
+
   /// 标题
   final String? label;
+
   /// 是否多选
   final bool? multiple;
+
   /// 选项数据
-  final List<TDDropdownItemOptions<T>> options;
+  final List<TDDropdownItemOptions> options;
+
   /// 选项分栏（1-3）
   final int? optionsColumns;
+
   /// 选中值
   final T? value;
+
   /// 值改变时触发
   final ValueChanged<T>? onChange;
+
   /// 点击确认时触发
   final ValueChanged<T>? onConfirm;
+
   /// 点击重置时触发
   final ValueChanged<T>? onReset;
 
   @override
   _TDDropdownItemState createState() => _TDDropdownItemState();
+
+  String getLabel() {
+    if (label != null) {
+      return label!;
+    }
+    try {
+      var option = options.firstWhere((element) => element.value == value);
+      return option.label;
+    } catch (e) {
+      return '';
+    }
+  }
 }
 
 class _TDDropdownItemState extends State<TDDropdownItem> {
-
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: const [],
-    );
+                  children: List.generate(
+                    10,
+                    (index) => Container(
+                      height: 50,
+                      color: index % 2 == 0 ? Colors.red : Colors.blue,
+                      child: Center(child: Text('Item $index')),
+                    ),
+                  ),
+                );
   }
-
-}
-
-/// 用来定义 value / label 在 options 中对应的字段别名
-class TDDropdownItemKeys {
-  const TDDropdownItemKeys({this.value = 'value', this.label = 'label'});
-  /// options的值的key
-  final String? value;
-  /// options的标题的key
-  final String? label;
 }
 
 /// 选项数据
-class TDDropdownItemOptions<T> {
-  const TDDropdownItemOptions({required this.value, required this.label, this.disabled = false});
-  /// 选项值
-  final T value;
+class TDDropdownItemOptions<E extends Object> {
+  const TDDropdownItemOptions(
+      {required this.value, required this.label, this.disabled = false});
+
+  /// 选项值，类型限制为int/String
+  final E value;
+
   /// 选项标题
   final String label;
+
   /// 是否禁用
   final bool? disabled;
 }
