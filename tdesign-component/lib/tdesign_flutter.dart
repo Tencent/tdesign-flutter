a10b3372b30f5da1de88140c8fe77fc88a24a05f
hkaikai <617760820@qq.com>
2024-05-16 17:45:46 +0800

    初始化组件

--- tdesign-component/lib/tdesign_flutter.dart
+++ tdesign-component/lib/tdesign_flutter.dart
@@ -8,6 +8,8 @@ export 'src/components/count_down/td_count_down.dart';
 export 'src/components/count_down/td_count_down_style.dart';
 export 'src/components/dialog/td_dialog.dart';
 export 'src/components/divider/td_divider.dart';
+export 'src/components/drawer/td_drawer.dart';
+export 'src/components/drawer/td_drawer_style.dart';
 export 'src/components/dropdown_menu/td_dropdown_item.dart';
 export 'src/components/dropdown_menu/td_dropdown_menu.dart';
 export 'src/components/empty/td_empty.dart';
