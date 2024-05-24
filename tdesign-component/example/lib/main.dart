3585ec0195ee947d12070fd845e0333e8159f46f
Luozf12345 <2515187986@qq.com>
2024-05-24 10:51:00 +0800

    ReadMe增加国际化官方文档链接

--- tdesign-component/example/lib/main.dart
+++ tdesign-component/example/lib/main.dart
@@ -56,7 +56,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(extensions: [_themeData], colorScheme: ColorScheme.light(primary: _themeData.brandNormalColor)),
       home: Builder(
         builder: (context) {
-          // 设置文案代理,国家化需要在MaterialApp初始化完成之后才生效,而且需要每次更新context
+          // 设置文案代理,国际化需要在MaterialApp初始化完成之后才生效,而且需要每次更新context
           TDTheme.setResourceBuilder((context) => delegate..updateContext(context), needAlwaysBuild: true);
           return MyHomePage(
             title: AppLocalizations.of(context)?.components ?? '',
