98519bdff1619d9557802f4bab1c58cbfee5ea45
ccXxx1aoBai <1426169428@qq.com>
2024-05-30 17:02:37 +0800

    支持垂直滚动、修改属性名称

--- tdesign-component/example/lib/page/td_notice_bar_page.dart
+++ tdesign-component/example/lib/page/td_notice_bar_page.dart
@@ -24,6 +24,7 @@ class TDNoticeBarPage extends StatelessWidget {
           ExampleItem(desc: '步进滚动通知', builder: _stepNoticeBar),
           ExampleItem(desc: '带图标滚动通知', builder: _scrollIconNoticeBar),
           ExampleItem(desc: '带图标步进通知', builder: _stepIconNoticeBar),
+          ExampleItem(desc: '带图标垂直步进通知', builder: _stepVerticalIconNoticeBar),
         ]),
         ExampleModule(title: '组件样式', children: [
           ExampleItem(desc: '背景色', builder: _setBgColorNoticeBar),
@@ -36,13 +37,13 @@ class TDNoticeBarPage extends StatelessWidget {
 
 @Demo(group: 'noticeBar')
 Widget _textNoticeBar(BuildContext context) {
-  return const TDNoticeBar(text: '这是静止的通知内容', type: TdNoticeBarType.none);
+  return const TDNoticeBar(context: '这是静止的通知内容', type: TdNoticeBarType.none);
 }
 
 @Demo(group: 'noticeBar')
 Widget _scrollNoticeBar(BuildContext context) {
   return const TDNoticeBar(
-    text: '这是一条滚动通知',
+    context: '这是一条滚动通知',
     type: TdNoticeBarType.scroll,
     duration: 2000,
   );
@@ -51,7 +52,7 @@ Widget _scrollNoticeBar(BuildContext context) {
 @Demo(group: 'noticeBar')
 Widget _stepNoticeBar(BuildContext context) {
   return const TDNoticeBar(
-    textList: ['这是第一条通知', '这是第二条通知', '这是第三条通知'],
+    contexts: ['这是第一条通知', '这是第二条通知', '这是第三条通知'],
     type: TdNoticeBarType.step,
     interval: 2000,
     duration: 1000,
@@ -61,7 +62,7 @@ Widget _stepNoticeBar(BuildContext context) {
 @Demo(group: 'noticeBar')
 Widget _scrollIconNoticeBar(BuildContext context) {
   return TDNoticeBar(
-    text: '这是一条滚动通知',
+    context: '这是一条滚动通知',
     duration: 2000,
     type: TdNoticeBarType.scroll,
     left: Icon(TDIcons.sound, color: TDTheme.of(context).brandNormalColor),
@@ -72,7 +73,7 @@ Widget _scrollIconNoticeBar(BuildContext context) {
 @Demo(group: 'noticeBar')
 Widget _stepIconNoticeBar(BuildContext context) {
   return TDNoticeBar(
-    textList: const ['这是第一条通知', '这是第二条通知', '这是第三条通知'],
+    contexts: const ['这是第一条通知', '这是第二条通知', '这是第三条通知'],
     type: TdNoticeBarType.step,
     interval: 2000,
     duration: 1000,
@@ -82,9 +83,22 @@ Widget _stepIconNoticeBar(BuildContext context) {
 }
 
 @Demo(group: 'noticeBar')
+Widget _stepVerticalIconNoticeBar(BuildContext context) {
+  return TDNoticeBar(
+    contexts: const ['这是第一条通知', '这是第二条通知', '这是第三条通知'],
+    type: TdNoticeBarType.step,
+    interval: 2000,
+    duration: 300,
+    direction: Axis.vertical,
+    left: Icon(TDIcons.sound, color: TDTheme.of(context).brandNormalColor),
+    right: Icon(TDIcons.chevron_right, color: TDTheme.of(context).grayColor8),
+  );
+}
+
+@Demo(group: 'noticeBar')
 Widget _setBgColorNoticeBar(BuildContext context) {
   return TDNoticeBar(
-    text: '这是一条滚动通知',
+    context: '这是一条滚动通知',
     textStyle: const TextStyle(color: Colors.white),
     duration: 2000,
     type: TdNoticeBarType.scroll,
@@ -95,7 +109,7 @@ Widget _setBgColorNoticeBar(BuildContext context) {
 @Demo(group: 'noticeBar')
 Widget _setFontSizeNoticeBar(BuildContext context) {
   return const TDNoticeBar(
-    text: '这是一条滚动通知',
+    context: '这是一条滚动通知',
     textStyle: TextStyle(fontSize: 24),
     duration: 3000,
     type: TdNoticeBarType.scroll,
