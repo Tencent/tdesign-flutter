f0319ccc2e18874fdc9d2f93811d25b06aeddd83
ccXxx1aoBai <1426169428@qq.com>
2024-06-07 18:13:50 +0800

    增加组件样式类

--- tdesign-component/lib/src/components/notice_bar/td_notice_bar.dart
+++ tdesign-component/lib/src/components/notice_bar/td_notice_bar.dart
@@ -1,41 +1,33 @@
 import 'dart:async';
+import 'dart:convert';
 
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-import '../icon/td_icons.dart';
-
-enum TdNoticeBarType {
-  none,
-  scroll,
-  step,
-}
+import '../../../tdesign_flutter.dart';
 
 /// 用于警告或提示
 class TDNoticeBar extends StatefulWidget {
   const TDNoticeBar({
     super.key,
     this.context,
-    this.contexts,
-    this.textStyle,
+    this.style,
     this.left,
     this.right,
-    this.backgroundColor = Colors.white,
-    this.padding = const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-    this.duration = 3000,
-    this.interval = 2000,
-    this.type = TdNoticeBarType.scroll,
+    this.speed = 50,
+    this.interval = 3000,
+    this.marquee = false,
     this.direction = Axis.horizontal,
+    this.theme = TDNoticeBarTheme.info,
+    this.prefixIcon,
+    this.suffixIcon,
   });
 
   /// 文本内容
-  final String? context;
-
-  /// 步进滚动内容
-  final List<String>? contexts;
+  final dynamic context;
 
   /// 文字样式
-  final TextStyle? textStyle;
+  final TDNoticeBarStyle? style;
 
   /// 左侧内容
   final Widget? left;
@@ -43,46 +35,55 @@ class TDNoticeBar extends StatefulWidget {
   /// 右侧内容
   final Widget? right;
 
-  /// 背景色
-  final Color? backgroundColor;
+  /// 跑马灯效果
+  final bool? marquee;
 
-  /// 内边距
-  final EdgeInsetsGeometry? padding;
-
-  /// 滚动周期（毫秒）
-  final int? duration;
+  /// 滚动速度
+  final double? speed;
 
   /// 步进滚动间隔时间（毫秒）
   final int? interval;
 
-  /// 滚动类型
-  final TdNoticeBarType? type;
-
   /// 滚动方向（步进可选）
   final Axis? direction;
+
+  /// 内置主题
+  final TDNoticeBarTheme? theme;
+
+  /// 前置图标
+  final IconData? prefixIcon;
+
+  /// 右侧图标
+  final IconData? suffixIcon;
 
   @override
   State<StatefulWidget> createState() => _TDNoticeBarState();
 }
 
 class _TDNoticeBarState extends State<TDNoticeBar> {
-  late ScrollController _scrollController;
-  final GlobalKey _key = GlobalKey();
+  ScrollController? _scrollController;
   Timer? _timer;
-  late Size _size;
+  Size? _size;
+  TDNoticeBarStyle? _style;
+  Color? _backgroundColor;
+  Widget? _left;
+  Widget? _right;
 
   @override
   void initState() {
     super.initState();
-    if (widget.duration! <= 0) {
-      throw Exception('duration must not be less than 0');
+    if (widget.speed! < 0) {
+      throw Exception('speed must not be less than 0');
     }
     if (widget.interval! <= 0) {
       throw Exception('stepDuration must not be less than 0');
     }
     _scrollController = ScrollController();
+    _init();
     WidgetsBinding.instance.addPostFrameCallback((time) {
-      _startTimer();
+      if (widget.marquee == true) {
+        _startTimer();
+      }
     });
   }
 
@@ -90,152 +91,152 @@ class _TDNoticeBarState extends State<TDNoticeBar> {
   void dispose() {
     super.dispose();
     _timer?.cancel();
-    _scrollController.dispose();
+    _scrollController?.dispose();
+  }
+
+  void _init() {
+    if (widget.style != null) {
+      _style = widget.style;
+    } else {
+      _style = TDNoticeBarStyle.generateTheme(context, theme: widget.theme);
+    }
+    _backgroundColor = _style!.backgroundColor;
+    _setLeftWidget();
+    _setRightWidget();
   }
 
   void _startTimer() {
-    if (widget.type == TdNoticeBarType.scroll) {
-      _scrollScroll();
-    } else if (widget.type == TdNoticeBarType.step) {
-      _stepScroll();
-    }
-  }
-
-  void _scrollScroll() {
-    if (widget.context == null) {
-      throw Exception('text must not be null when type is scroll');
-    }
-    if (widget.direction == Axis.vertical) {
-      throw Exception('direction must be horizontal when type is scroll');
-    }
-    _scrollController.jumpTo(0);
-    _scrollController.animateTo(_size.width,
-        duration: Duration(milliseconds: widget.duration!),
-        curve: Curves.linear);
-    _timer =
-        Timer.periodic(Duration(milliseconds: widget.duration!), (Timer timer) {
-      _scrollController.jumpTo(0);
-      _scrollController.animateTo(_size.width,
-          duration: Duration(milliseconds: widget.duration!),
-          curve: Curves.linear);
-    });
-  }
-
-  Future<void> _stepScroll() async {
-    if (widget.contexts == null || widget.contexts!.isEmpty) {
-      throw Exception('textList must not be null when type is step');
-    }
-    var index = 0;
-    var offset = 0.0;
-    var scrollDistance =
-        widget.direction == Axis.horizontal ? _size.width : _getFontSize();
-    _scrollController.jumpTo(0);
-    await Future.delayed(Duration(milliseconds: widget.interval!), () {
-      offset += scrollDistance;
-      _scrollController.animateTo(offset,
-          duration: Duration(milliseconds: widget.duration!),
-          curve: Curves.linear);
-      index++;
-    });
-    _timer = Timer.periodic(
-        Duration(milliseconds: widget.interval! + widget.duration!),
-        (Timer timer) {
-      if (index > widget.contexts!.length - 1) {
-        index = 0;
-        offset = 0;
-        _scrollController.jumpTo(0);
-      }
-      offset += scrollDistance;
-      index++;
-      _scrollController.animateTo(offset,
-          duration: Duration(milliseconds: widget.duration!),
-          curve: Curves.linear);
-    });
-  }
-
-  Widget _buildWidget() {
     if (widget.direction == Axis.horizontal) {
-      return Row(children: _children());
+      _scroll();
+    } else if (widget.direction == Axis.vertical) {
+      _step();
     }
-    return Column(children: _children());
   }
 
-  List<Widget> _children() {
-    if (widget.contexts != null) {
-      var items = <Widget>[];
-      for (var i = 0; i < widget.contexts!.length; i++) {
-        items.add(
-          SizedBox(
-            width: _size.width,
-            child: Text(
-              widget.contexts![i],
-              style: widget.textStyle,
-            ),
-          ),
-        );
-      }
-      items.add(SizedBox(width: _size.width, child: Text(widget.contexts![0])));
-      return items;
+  void _scroll() {
+    var scrollDistance = _getContextWidth() + _size!.width;
+    if (_getContextWidth() > _size!.width) {
+      scrollDistance = _getContextWidth() + _getEmptyWidth();
     }
-    return [
-      SizedBox(
-        width: _size.width,
-        child: Text(
-          widget.context!,
-          style: widget.textStyle,
-        ),
-      ),
-      SizedBox(
-        width: _size.width,
-        child: Text(
-          widget.context!,
-          style: widget.textStyle,
-        ),
-      ),
-    ];
+    _scrollController!.jumpTo(0);
+    var offset = 0.0 + widget.speed!;
+    _scrollController!.animateTo(offset,
+        duration: const Duration(seconds: 1), curve: Curves.linear);
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) async {
+      offset += widget.speed!;
+      if (offset >= scrollDistance) {
+        // 最后一阶段未滚动距离转化时间比（毫秒数）
+        offset = 50;
+        _scrollController!.jumpTo(0);
+      }
+      _scrollController!.animateTo(offset,
+          duration: const Duration(seconds: 1), curve: Curves.linear);
+    });
   }
+
+  void _step() {}
 
   double _getFontSize() {
-    final textPainter = TextPainter(
-      text: TextSpan(
-        text: widget.context ?? widget.contexts![0],
-        style: widget.textStyle,
-      ),
-      textDirection: TextDirection.ltr,
-    )..layout(maxWidth: _size.width);
-    return textPainter.size.height;
+    return _style!.getTextStyle.fontSize ?? 16;
+  }
+
+  void _setLeftWidget() {
+    if (widget.prefixIcon != null) {
+      _left = Icon(
+        widget.prefixIcon,
+        color: widget.style?.leftIconColor,
+      );
+    }
+    if (widget.left != null) {
+      _left = widget.left;
+    }
+  }
+
+  void _setRightWidget() {
+    if (widget.suffixIcon != null) {
+      _right = Icon(
+        widget.suffixIcon,
+        color: widget.style?.rightIconColor,
+      );
+    }
+    if (widget.right != null) {
+      _right = widget.right;
+    }
+  }
+
+  double _getContextWidth() {
+    var contextWidth = _getFontSize() * widget.context!.length;
+    if (contextWidth > _size!.width) {
+      return contextWidth;
+    }
+    return _size!.width;
+  }
+
+  double _getEmptyWidth() {
+    print('32ssss::::::::::${_style!.getPadding}:::${_style!.getPadding.horizontal}');
+    return _size!.width - _style!.getPadding.horizontal;
+  }
+
+  Widget _contextWidget() {
+    Widget textWidget = TDText(widget.context, style: _style?.getTextStyle);
+    Widget? child = textWidget;
+    switch (widget.direction) {
+      case Axis.horizontal:
+        child = SingleChildScrollView(
+          controller: _scrollController,
+          scrollDirection: widget.direction!,
+          // physics: const NeverScrollableScrollPhysics(),
+          child: Row(
+            children: [
+              SizedBox(
+                width: _getContextWidth(),
+                height: _getFontSize(),
+                child: textWidget,
+              ),
+              SizedBox(width: _getEmptyWidth()),
+              SizedBox(
+                width: _getContextWidth(),
+                height: _getFontSize(),
+                child: textWidget,
+              )
+            ],
+          ),
+        );
+        break;
+      case Axis.vertical:
+        break;
+      default:
+        child = textWidget;
+        break;
+    }
+    return child;
   }
 
   @override
   Widget build(BuildContext context) {
     _size = MediaQuery.of(context).size;
     return Container(
-      height: _getFontSize() + widget.padding!.vertical,
-      padding: widget.padding,
+      width: _size!.width,
+      height: _getFontSize() + _style!.getPadding.vertical,
+      padding: _style!.getPadding,
       decoration: BoxDecoration(
-        color: widget.backgroundColor,
+        color: _backgroundColor,
       ),
       child: Row(
         children: [
           Visibility(
-            visible: widget.left != null,
+            visible: _left != null,
             child: Container(
-              margin: const EdgeInsets.only(right: 8),
-              child: widget.left,
+              child: _left,
             ),
           ),
           Expanded(
-              key: _key,
-              child: SingleChildScrollView(
-                  scrollDirection: widget.direction!,
-                  controller: _scrollController,
-                  physics: const NeverScrollableScrollPhysics(),
-                  child: _buildWidget())),
+            child: _contextWidget(),
+          ),
           Visibility(
-            visible: widget.right != null,
+            visible: _right != null,
             child: Container(
-              margin: const EdgeInsets.only(left: 8),
-              child: widget.right,
+              child: _right,
             ),
           ),
         ],
