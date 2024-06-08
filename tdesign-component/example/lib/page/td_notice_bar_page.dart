f03252973a30f1033e331abc05818f5de1638438
ccXxx1aoBai <1426169428@qq.com>
2024-06-08 11:18:29 +0800

    noticeBar组件重构

--- tdesign-component/example/lib/page/td_notice_bar_page.dart
+++ tdesign-component/example/lib/page/td_notice_bar_page.dart
@@ -2,7 +2,9 @@
 /// @Author lwb
 /// @Date 2024/5/28
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:tdesign_flutter/tdesign_flutter.dart';
 
 import '../annotation/demo.dart';
@@ -21,15 +23,27 @@ class TDNoticeBarPage extends StatelessWidget {
         ExampleModule(title: '组件类型', children: [
           ExampleItem(desc: '纯文字的公告栏', builder: _textNoticeBar),
           ExampleItem(desc: '可滚动的公告栏', builder: _scrollNoticeBar),
-          // ExampleItem(builder: _scrollIconNoticeBar),
-          // ExampleItem(desc: '步进滚动通知', builder: _stepNoticeBar),
-          // ExampleItem(desc: '带图标步进通知', builder: _stepIconNoticeBar),
-          // ExampleItem(desc: '带图标垂直步进通知', builder: _stepVerticalIconNoticeBar),
+          ExampleItem(builder: _scrollIconNoticeBar),
+          ExampleItem(desc: '带图标的公告栏', builder: _iconNoticeBar),
+          ExampleItem(desc: '带关闭的公告栏', builder: _closeNoticeBar),
+          ExampleItem(desc: '带入口的公告栏', builder: _entranceNoticeBar1),
+          ExampleItem(builder: _entranceNoticeBar2),
+          ExampleItem(desc: '自定义样式的公告栏', builder: _customNoticeBar),
+        ]),
+        ExampleModule(title: '组件状态', children: [
+          ExampleItem(desc: '普通通知', builder: _normalNoticeBar),
+          ExampleItem(desc: '成功通知', builder: _successNoticeBar),
+          ExampleItem(desc: '警示通知', builder: _warningNoticeBar),
+          ExampleItem(desc: '错误通知', builder: _errorNoticeBar),
         ]),
         ExampleModule(title: '组件样式', children: [
-          // ExampleItem(desc: '背景色', builder: _setBgColorNoticeBar),
-          // ExampleItem(desc: '文字大小', builder: _setFontSizeNoticeBar),
+          ExampleItem(desc: '卡片顶部', builder: _cardNoticeBar),
         ])
+      ],
+      test: [
+        ExampleItem(desc: '带点击事件的公告栏', builder: _tapNoticeBar),
+        ExampleItem(desc: '自定义左侧内容的公告栏', builder: _leftNoticeBar),
+        ExampleItem(desc: '垂直滚动的公告栏', builder: _stepNoticeBar),
       ],
     );
   }
@@ -50,63 +64,193 @@ Widget _scrollNoticeBar(BuildContext context) {
 }
 
 @Demo(group: 'noticeBar')
-Widget _stepNoticeBar(BuildContext context) {
-  return const TDNoticeBar(
-    context: ['这是第一条通知', '这是第二条通知', '这是第三条通知'],
-    interval: 2000,
-    speed: 50,
-  );
-}
-
-@Demo(group: 'noticeBar')
 Widget _scrollIconNoticeBar(BuildContext context) {
-  return Padding(
-    padding: const EdgeInsets.only(top: 16),
+  return const Padding(
+    padding: EdgeInsets.only(top: 16),
     child: TDNoticeBar(
-      context: '这是一条滚动通知',
+      context: '提示文字描述提示文字描述提示文字描述提示文字描述提示文字',
       speed: 50,
-      left: Icon(TDIcons.sound, color: TDTheme.of(context).brandNormalColor),
+      prefixIcon: TDIcons.sound,
+      marquee: true,
     ),
   );
 }
 
 @Demo(group: 'noticeBar')
-Widget _stepIconNoticeBar(BuildContext context) {
-  return TDNoticeBar(
-    context: const ['这是第一条通知', '这是第二条通知', '这是第三条通知'],
-    interval: 2000,
-    speed: 50,
-    left: Icon(TDIcons.sound, color: TDTheme.of(context).brandNormalColor),
-    right: Icon(TDIcons.chevron_right, color: TDTheme.of(context).grayColor8),
+Widget _iconNoticeBar(BuildContext context) {
+  return const TDNoticeBar(
+    context: '这是一条普通的通知信息',
+    prefixIcon: TDIcons.error_circle_filled,
   );
 }
 
 @Demo(group: 'noticeBar')
-Widget _stepVerticalIconNoticeBar(BuildContext context) {
+Widget _closeNoticeBar(BuildContext context) {
+  return const TDNoticeBar(
+    context: '这是一条普通的通知信息',
+    prefixIcon: TDIcons.error_circle_filled,
+    suffixIcon: TDIcons.close,
+  );
+}
+
+@Demo(group: 'noticeBar')
+Widget _entranceNoticeBar1(BuildContext context) {
+  return const TDNoticeBar(
+    context: '这是一条普通的通知信息',
+    prefixIcon: TDIcons.error_circle_filled,
+    right: TDButton(
+      text: '文字按钮',
+      type: TDButtonType.text,
+      theme: TDButtonTheme.primary,
+    ),
+  );
+}
+
+@Demo(group: 'noticeBar')
+Widget _entranceNoticeBar2(BuildContext context) {
+  return const Padding(
+    padding: EdgeInsets.only(top: 16),
+    child: TDNoticeBar(
+      context: '这是一条普通的通知信息',
+      prefixIcon: TDIcons.error_circle_filled,
+      suffixIcon: TDIcons.chevron_right,
+    ),
+  );
+}
+
+@Demo(group: 'noticeBar')
+Widget _customNoticeBar(BuildContext context) {
   return TDNoticeBar(
-    context: const ['这是第一条通知', '这是第二条通知', '这是第三条通知'],
-    interval: 2000,
-    speed: 300,
+    context: '这是一条普通的通知信息',
+    prefixIcon: TDIcons.notification,
+    suffixIcon: TDIcons.chevron_right,
+    style: TDNoticeBarStyle(backgroundColor: TDTheme.of(context).grayColor3),
+  );
+}
+
+@Demo(group: 'noticeBar')
+Widget _normalNoticeBar(BuildContext context) {
+  return const TDNoticeBar(
+    context: '这是一条普通的通知信息',
+    prefixIcon: TDIcons.error_circle_filled,
+    theme: TDNoticeBarTheme.info,
+  );
+}
+
+@Demo(group: 'noticeBar')
+Widget _successNoticeBar(BuildContext context) {
+  return const TDNoticeBar(
+    context: '这是一条普通的通知信息',
+    prefixIcon: TDIcons.error_circle_filled,
+    theme: TDNoticeBarTheme.success,
+  );
+}
+
+@Demo(group: 'noticeBar')
+Widget _warningNoticeBar(BuildContext context) {
+  return const TDNoticeBar(
+    context: '这是一条普通的通知信息',
+    prefixIcon: TDIcons.error_circle_filled,
+    theme: TDNoticeBarTheme.warning,
+  );
+}
+
+@Demo(group: 'noticeBar')
+Widget _errorNoticeBar(BuildContext context) {
+  return const TDNoticeBar(
+    context: '这是一条普通的通知信息',
+    prefixIcon: TDIcons.error_circle_filled,
+    theme: TDNoticeBarTheme.error,
+  );
+}
+
+@Demo(group: 'noticeBar')
+Widget _cardNoticeBar(BuildContext context) {
+  var size = MediaQuery.of(context).size;
+  return Container(
+    margin: const EdgeInsets.symmetric(horizontal: 16),
+    decoration: BoxDecoration(
+      color: TDNoticeBarStyle.generateTheme(context).backgroundColor,
+      borderRadius: const BorderRadius.all(Radius.circular(9)),
+      boxShadow: const [
+        BoxShadow(
+          color: Color(0x0d000000),
+          blurRadius: 8,
+          spreadRadius: 2,
+          offset: Offset(0, 2),
+        ),
+        BoxShadow(
+          color: Color(0x0f000000),
+          blurRadius: 10,
+          spreadRadius: 1,
+          offset: Offset(0, 8),
+        ),
+        BoxShadow(
+          color: Color(0x1a000000),
+          blurRadius: 5,
+          spreadRadius: -3,
+          offset: Offset(0, 5),
+        ),
+      ],
+    ),
+    child: Column(
+      children: [
+        Container(
+          width: size.width - 32,
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+          ),
+          clipBehavior: Clip.hardEdge,
+          child: const TDNoticeBar(
+            context: '这是一条普通的通知信息',
+            prefixIcon: TDIcons.error_circle_filled,
+            suffixIcon: TDIcons.chevron_right,
+          ),
+        ),
+        Container(
+          height: 150,
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+          ),
+        )
+      ],
+    ),
+  );
+}
+
+@Demo(group: 'noticeBar')
+Widget _tapNoticeBar(BuildContext context) {
+  return TDNoticeBar(
+    context: '这是一条普通的通知信息',
+    prefixIcon: TDIcons.error_circle_filled,
+    suffixIcon: TDIcons.chevron_right,
+    onTap: (trigger) {
+      TDToast.showText('tap:$trigger', context: context);
+    },
+  );
+}
+
+@Demo(group: 'noticeBar')
+Widget _leftNoticeBar(BuildContext context) {
+  return const TDNoticeBar(
+    context: '这是一条普通的通知信息',
+    suffixIcon: TDIcons.chevron_right,
+    left: TDButton(
+      text: '文本',
+      type: TDButtonType.text,
+      theme: TDButtonTheme.primary,
+      size: TDButtonSize.small,
+    ),
+  );
+}
+
+@Demo(group: 'noticeBar')
+Widget _stepNoticeBar(BuildContext context) {
+  return const TDNoticeBar(
+    context: ['君不见黄河之水天上来', '奔流到海不复回', '君不见'],
     direction: Axis.vertical,
-    left: Icon(TDIcons.sound, color: TDTheme.of(context).brandNormalColor),
-    right: Icon(TDIcons.chevron_right, color: TDTheme.of(context).grayColor8),
-  );
-}
-
-@Demo(group: 'noticeBar')
-Widget _setBgColorNoticeBar(BuildContext context) {
-  return TDNoticeBar(
-    context: '这是一条滚动通知',
-    style: TDNoticeBarStyle(textStyle: const TextStyle(color: Colors.white)),
-    speed: 50,
-  );
-}
-
-@Demo(group: 'noticeBar')
-Widget _setFontSizeNoticeBar(BuildContext context) {
-  return TDNoticeBar(
-    context: '这是一条滚动通知',
-    style: TDNoticeBarStyle(textStyle: const TextStyle(fontSize: 24)),
-    speed: 50,
+    prefixIcon: TDIcons.sound,
+    marquee: true,
   );
 }
