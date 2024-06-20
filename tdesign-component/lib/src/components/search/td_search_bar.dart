d77e665764adc3de881256b19e343034d41d7898
v_szheshi <v_szheshi@tencent.com>
2024-06-20 11:25:28 +0800

    新增控制器

--- tdesign-component/lib/src/components/search/td_search_bar.dart
+++ tdesign-component/lib/src/components/search/td_search_bar.dart
@@ -36,6 +36,7 @@ class TDSearchBar extends StatefulWidget {
     this.autoFocus = false,
     this.mediumStyle = false,
     this.needCancel = false,
+    this.controller,
     this.backgroundColor = Colors.white,
   }) : super(key: key);
 
@@ -65,6 +66,9 @@ class TDSearchBar extends StatefulWidget {
 
   /// 是否需要取消按钮
   final bool needCancel;
+
+  /// 控制器
+  final TextEditingController? controller;
 
   /// 文字改变回调
   final TDSearchBarEvent? onTextChanged;
@@ -104,10 +108,17 @@ class _TDSearchBarState extends State<TDSearchBar>
     _status = widget.autoFocus
         ? _TDSearchBarStatus.focused
         : _TDSearchBarStatus.unFocus;
-    controller.addListener(() {
-      var clearVisible = controller.text.isNotEmpty;
-      _updateClearBtnVisible(clearVisible);
-    });
+    if(widget.controller==null){
+      controller.addListener(() {
+        var clearVisible = controller.text.isNotEmpty;
+        _updateClearBtnVisible(clearVisible!);
+      });
+    }else{
+      widget.controller?.addListener(() {
+        var clearVisible = widget.controller?.text.isNotEmpty;
+        _updateClearBtnVisible(clearVisible!);
+      });
+    }
     focusNode.addListener(() {
       setState(() {
         _status = focusNode.hasFocus
@@ -161,6 +172,14 @@ class _TDSearchBarState extends State<TDSearchBar>
     });
   }
 
+  void _cleanInputText(){
+    if(widget.controller==null){
+      controller.clear();
+    }else{
+      widget.controller?.clear();
+    }
+  }
+
   Font? getSize(BuildContext context) {
     return widget.mediumStyle
         ? TDTheme.of(context).fontBodyMedium
@@ -202,7 +221,7 @@ class _TDSearchBarState extends State<TDSearchBar>
                         margin: const EdgeInsets.only(bottom: 1),// 为了适配TextField与Text的差异，后续需要做通用适配
                         child: TextField(
                           key: _textFieldKey,
-                          controller: controller,
+                          controller: widget.controller??controller,
                           autofocus: widget.autoFocus,
                           cursorColor: TDTheme.of(context).brandNormalColor,
                           cursorWidth: 1,
@@ -237,7 +256,7 @@ class _TDSearchBarState extends State<TDSearchBar>
                       offstage: clearBtnHide,
                       child: GestureDetector(
                           onTap: () {
-                            controller.clear();
+                            _cleanInputText();
                             if (widget.onTextChanged != null) {
                               widget.onTextChanged!('');
                             }
@@ -257,7 +276,7 @@ class _TDSearchBarState extends State<TDSearchBar>
               offstage: cancelBtnHide || !widget.needCancel,
               child: GestureDetector(
                 onTap: () {
-                  controller.clear();
+                  _cleanInputText();
                   if (widget.onTextChanged != null) {
                     widget.onTextChanged!('');
                   }
@@ -287,8 +306,8 @@ class _TDSearchBarState extends State<TDSearchBar>
           ],
         ),
         Offstage(
-          offstage: (_status == _TDSearchBarStatus.focused ||
-              controller.text.isNotEmpty),
+        offstage:widget.controller==null?(_status == _TDSearchBarStatus.focused ||
+        controller.text.isNotEmpty):(_status == _TDSearchBarStatus.focused ||widget.controller!.text.isNotEmpty),
           child: GestureDetector(
               onTap: () {
                 if (_status == _TDSearchBarStatus.animatingToFocus ||
