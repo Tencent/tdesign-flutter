3287a0ec66ed03dc68d1ae152e9c05b0c6218f62
aaronmhl <297546700@qq.com>
2024-06-23 10:34:52 +0800

    refactor(feature/steps): update steps component example

--- tdesign-component/example/lib/page/td_steps_page.dart
+++ tdesign-component/example/lib/page/td_steps_page.dart
@@ -20,29 +20,50 @@ class _TDStepsPageState extends State<TDStepsPage> {
       exampleCodeGroup: 'steps',
       desc: 'steps步骤条',
       children: [
-        ExampleModule(title: '组件类型', children: [
+        ExampleModule(title: '水平默认步骤条', children: [
           ExampleItem(
-              desc: '基本步骤',
-              builder: _buildBasicSteps),
+              desc: '水平默认步骤条1',
+              builder: _buildBasicHSteps1),
+          ExampleItem(
+              desc: '水平默认步骤条2',
+              builder: _buildBasicHSteps2),
+          ExampleItem(
+              desc: '水平默认步骤条3',
+              builder: _buildBasicHSteps3),
         ]),
-        ExampleModule(title: '组件类型', children: [
+        ExampleModule(title: '水平图标步骤条', children: [
           ExampleItem(
-              desc: '简略步骤',
-              builder: _buildSimpleSteps),
+              desc: '水平图标步骤条1',
+              builder: _buildHIconSteps1),
+          ExampleItem(
+              desc: '水平图标步骤条2',
+              builder: _buildHIconSteps2),
+          ExampleItem(
+              desc: '水平图标步骤条3',
+              builder: _buildHIconSteps3),
+        ]),
+        ExampleModule(title: '水平简略步骤条', children: [
+          ExampleItem(
+              desc: '水平简略步骤条1',
+              builder: _buildSimpleHSteps1),
+          ExampleItem(
+              desc: '水平简略步骤条2',
+              builder: _buildSimpleHSteps2),
+          ExampleItem(
+              desc: '水平简略步骤条3',
+              builder: _buildSimpleHSteps3),
         ]),
       ],
     );
   }
 
-  List<StepsItemData> basicStepsListData = [
-    StepsItemData(title: 'steps1', content: 'content1', successIcon: TDIcons.call),
+  List<StepsItemData> basicHStepsListData1 = [
+    StepsItemData(title: 'steps1', content: 'content1'),
     StepsItemData(title: 'steps2', content: 'content2'),
-    StepsItemData(title: 'steps3', content: 'content3', successIcon: TDIcons.call),
-    StepsItemData(title: 'steps4', content: 'content4', successIcon: TDIcons.call),
   ];
   /// 基本步骤1
   @Demo(group: 'steps')
-  Widget _buildBasicSteps(BuildContext context){
+  Widget _buildBasicHSteps1(BuildContext context){
     return Padding(
       padding: const EdgeInsets.only(left: 16, right: 16),
       child: Row(
@@ -50,9 +71,64 @@ class _TDStepsPageState extends State<TDStepsPage> {
           Expanded(
             flex: 1,
             child: TDSteps(
-              steps: basicStepsListData,
+              steps: basicHStepsListData1,
               direction: StatusDirection.horizontal,
-              activeIndex: 2,
+              activeIndex: 0,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+  List<StepsItemData> basicHStepsListData2 = [
+    StepsItemData(title: 'steps1', content: 'content1'),
+    StepsItemData(title: 'steps2', content: 'content2'),
+    StepsItemData(title: 'steps3', content: 'content3'),
+  ];
+  /// 基本步骤2
+  @Demo(group: 'steps')
+  Widget _buildBasicHSteps2(BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: TDSteps(
+              steps: basicHStepsListData2,
+              direction: StatusDirection.horizontal,
+              activeIndex: 1,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+  List<StepsItemData> basicHStepsListData3 = [
+    StepsItemData(title: 'steps1', content: 'content1'),
+    StepsItemData(title: 'steps2', content: 'content2'),
+    StepsItemData(title: 'steps3', content: 'content3'),
+    StepsItemData(title: 'steps4', content: 'content4'),
+  ];
+  List<StepsItemData> basicStepsListData4 = [
+    StepsItemData(title: 'steps1', content: 'content1', successIcon: TDIcons.call),
+    StepsItemData(title: 'steps2', content: 'content2'),
+    StepsItemData(title: 'steps3', content: 'content3', successIcon: TDIcons.call),
+    StepsItemData(title: 'steps4', content: 'content4', successIcon: TDIcons.call),
+  ];
+  /// 基本步骤3
+  @Demo(group: 'steps')
+  Widget _buildBasicHSteps3(BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: TDSteps(
+              steps: basicHStepsListData3,
+              direction: StatusDirection.horizontal,
+              activeIndex: 1,
             ),
           )
         ],
@@ -60,15 +136,13 @@ class _TDStepsPageState extends State<TDStepsPage> {
     );
   }
 
-  List<StepsItemData> simpleStepsListData = [
+  List<StepsItemData> hIconStepsListData1 = [
     StepsItemData(title: 'steps1', content: 'content1', successIcon: TDIcons.call),
-    StepsItemData(title: 'steps2', content: 'content2'),
-    StepsItemData(title: 'steps3', content: 'content3', successIcon: TDIcons.call),
-    StepsItemData(title: 'steps4', content: 'content4'),
+    StepsItemData(title: 'steps2', content: 'content2', successIcon: TDIcons.call),
   ];
-  /// 基本步骤2
+  /// 水平图标步骤条1
   @Demo(group: 'steps')
-  Widget _buildSimpleSteps(BuildContext context){
+  Widget _buildHIconSteps1(BuildContext context){
     return Padding(
       padding: const EdgeInsets.only(left: 16, right: 16),
       child: Row(
@@ -76,7 +150,135 @@ class _TDStepsPageState extends State<TDStepsPage> {
           Expanded(
             flex: 1,
             child: TDSteps(
-              steps: simpleStepsListData,
+              steps: hIconStepsListData1,
+              direction: StatusDirection.horizontal,
+              activeIndex: 0,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  List<StepsItemData> hIconStepsListData2 = [
+    StepsItemData(title: 'steps1', content: 'content1', successIcon: TDIcons.call),
+    StepsItemData(title: 'steps2', content: 'content2', successIcon: TDIcons.call),
+    StepsItemData(title: 'steps2', content: 'content2', successIcon: TDIcons.call),
+  ];
+  /// 水平图标步骤条1
+  @Demo(group: 'steps')
+  Widget _buildHIconSteps2(BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: TDSteps(
+              steps: hIconStepsListData2,
+              direction: StatusDirection.horizontal,
+              activeIndex: 1,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  List<StepsItemData> hIconStepsListData3 = [
+    StepsItemData(title: 'steps1', content: 'content1', successIcon: TDIcons.call),
+    StepsItemData(title: 'steps2', content: 'content2', successIcon: TDIcons.call),
+    StepsItemData(title: 'steps2', content: 'content2', successIcon: TDIcons.call),
+    StepsItemData(title: 'steps2', content: 'content2', successIcon: TDIcons.call),
+  ];
+  /// 水平图标步骤条1
+  @Demo(group: 'steps')
+  Widget _buildHIconSteps3(BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: TDSteps(
+              steps: hIconStepsListData3,
+              direction: StatusDirection.horizontal,
+              activeIndex: 1,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  List<StepsItemData> simpleHStepsListData1 = [
+    StepsItemData(title: 'steps1', content: 'content1'),
+    StepsItemData(title: 'steps2', content: 'content2'),
+  ];
+  /// 水平简略步骤条1
+  @Demo(group: 'steps')
+  Widget _buildSimpleHSteps1(BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: TDSteps(
+              steps: simpleHStepsListData1,
+              direction: StatusDirection.horizontal,
+              activeIndex: 0,
+              simple: true,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  List<StepsItemData> simpleHStepsListData2 = [
+    StepsItemData(title: 'steps1', content: 'content1'),
+    StepsItemData(title: 'steps2', content: 'content2'),
+    StepsItemData(title: 'steps3', content: 'content3'),
+  ];
+  /// 水平简略步骤条2
+  @Demo(group: 'steps')
+  Widget _buildSimpleHSteps2(BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: TDSteps(
+              steps: simpleHStepsListData2,
+              direction: StatusDirection.horizontal,
+              activeIndex: 1,
+              simple: true,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  List<StepsItemData> simpleHStepsListData3 = [
+    StepsItemData(title: 'steps1', content: 'content1'),
+    StepsItemData(title: 'steps2', content: 'content2'),
+    StepsItemData(title: 'steps3', content: 'content3'),
+    StepsItemData(title: 'steps3', content: 'content3'),
+  ];
+  /// 水平简略步骤条3
+  @Demo(group: 'steps')
+  Widget _buildSimpleHSteps3(BuildContext context){
+    return Padding(
+      padding: const EdgeInsets.only(left: 16, right: 16),
+      child: Row(
+        children: [
+          Expanded(
+            flex: 1,
+            child: TDSteps(
+              steps: simpleHStepsListData3,
               direction: StatusDirection.horizontal,
               activeIndex: 1,
               simple: true,
