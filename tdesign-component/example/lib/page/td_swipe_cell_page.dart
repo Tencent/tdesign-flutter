b8c6078747d470626610d8ef13b4674a51f9c082
hkaikai <617760820@qq.com>
2024-06-25 20:49:12 +0800

    TDSwipeCell组件开发

---
+++ tdesign-component/example/lib/page/td_swipe_cell_page.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:tdesign_flutter/tdesign_flutter.dart';
+
+import '../annotation/demo.dart';
+import '../base/example_widget.dart';
+
+class TDSwipeCellPage extends StatelessWidget {
+  const TDSwipeCellPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ExamplePage(
+      title: tdTitle(context),
+      exampleCodeGroup: 'swiperCell',
+      children: [
+        ExampleModule(
+          title: '组件类型',
+          children: [
+            ExampleItem(
+                desc: '用于承载列表中的更多操作，通过左右滑动来展示，按钮的宽度固定高度根据列表高度而变化。',
+                ignoreCode: true,
+                builder: (_) {
+                  return _buildSwiperCell(context);
+                }),
+          ],
+        ),
+      ],
+      test: const [],
+    );
+  }
+
+  // var cl = false;
+
+  @Demo(group: 'swiperCell')
+  Widget _buildSwiperCell(BuildContext context) {
+    return const _Demo();
+  }
+}
+
+class _Demo extends StatefulWidget {
+  const _Demo({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _DemoState createState() => _DemoState();
+}
+
+class _DemoState extends State<_Demo> {
+  @override
+  Widget build(BuildContext context) {
+    return TDSwipeAutoClose(
+        child: Column(
+      children: [
+        TDSwipeCell(
+          groupTag: '0',
+          opened: const [true],
+          onChange: (direction, open) {
+            print(direction);
+            print(open);
+          },
+          left: TDSwipePanel(
+            dragDismissible: true,
+            children: [
+              const TDSwipeAction(
+                backgroundColor: Color.fromARGB(255, 218, 16, 16),
+                label: '删除',
+                icon: TDIcons.delete,
+                direction: Axis.vertical,
+              ),
+              const TDSwipeAction(
+                backgroundColor: Color.fromARGB(255, 17, 50, 212),
+                label: '收藏',
+                icon: TDIcons.user_avatar,
+                // direction: Axis.vertical,
+              ),
+            ],
+          ),
+          right: TDSwipePanel(
+            // dragDismissible: true,
+            children: [
+              const TDSwipeAction(
+                backgroundColor: Color.fromARGB(255, 218, 16, 16),
+                label: '删除',
+                icon: TDIcons.delete,
+                direction: Axis.vertical,
+              ),
+              const TDSwipeAction(
+                backgroundColor: Color.fromARGB(255, 17, 50, 212),
+                label: '收藏',
+                icon: TDIcons.user_avatar,
+                // direction: Axis.vertical,
+              ),
+            ],
+          ),
+          cell: const ListTile(title: Text('Slide me')),
+        ),
+        TDSwipeCell(
+          groupTag: '0',
+          // opened: const [true],
+          onChange: (direction, open) {
+            print(direction);
+            print(open);
+          },
+          left: TDSwipePanel(
+            // dragDismissible: true,
+            children: [
+              const TDSwipeAction(
+                backgroundColor: Color.fromARGB(255, 218, 16, 16),
+                label: '删除',
+                icon: TDIcons.delete,
+                direction: Axis.vertical,
+              ),
+              const TDSwipeAction(
+                backgroundColor: Color.fromARGB(255, 17, 50, 212),
+                label: '收藏',
+                icon: TDIcons.user_avatar,
+                // direction: Axis.vertical,
+              ),
+            ],
+          ),
+          right: TDSwipePanel(
+            // dragDismissible: true,
+            children: [
+              const TDSwipeAction(
+                backgroundColor: Color.fromARGB(255, 218, 16, 16),
+                label: '删除',
+                icon: TDIcons.delete,
+                direction: Axis.vertical,
+              ),
+              const TDSwipeAction(
+                backgroundColor: Color.fromARGB(255, 17, 50, 212),
+                label: '收藏',
+                icon: TDIcons.user_avatar,
+                // direction: Axis.vertical,
+              ),
+            ],
+          ),
+          cell: const ListTile(title: Text('Slide me')),
+        ),
+      ],
+    ));
+  }
+}
--- tdesign-component/lib/src/components/swipe_cell/td_swipe_cell.dart
+++ tdesign-component/lib/src/components/swipe_cell/td_swipe_cell.dart
@@ -1,95 +1,140 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
-import '../../../tdesign_flutter.dart';
+
+import './td_swipe_panel.dart';
 import '../cell/td_cell.dart';
-import 'td_swipe_option.dart';
 
-typedef CellBuilder = Widget Function(BuildContext context, TDCell cell);
-
-enum SwipeDirection { right, left }
+enum TDSwipeDirection { right, left }
 
 /// 滑动单元格组件
 class TDSwipeCell extends StatefulWidget {
   const TDSwipeCell({
     Key? key,
-    this.cell,
-    this.cells,
-    this.builder,
+    this.slidableKey,
+    required this.cell,
     this.disabled = false,
-    this.openedRight = false,
-    this.openedLeft = false,
+    this.opened = const [false, false],
     this.right,
-    this.rightWidget,
-    this.rightConfirm,
-    this.rightConfirmWidget,
     this.left,
-    this.leftWidget,
-    this.leftConfirm,
-    this.leftConfirmWidget,
     this.onChange,
     this.controller,
     this.groupTag,
+    this.closeOnScroll = true,
+    this.dragStartBehavior = DragStartBehavior.start,
+    this.direction = Axis.horizontal,
   }) : super(key: key);
 
-  /// 单元格
-  final TDCell? cell;
+  /// 滑动组件的 Key
+  final Key? slidableKey;
 
-  /// 单元格列表，优先级高于cell
-  final List<TDCell>? cells;
-
-  /// cell构建器，可自定义cell父组件，如Dismissible
-  final CellBuilder? builder;
+  /// 单元格 [TDCell]
+  final Widget cell;
 
   /// 是否禁用滑动
   final bool? disabled;
 
-  /// 默认打开右侧
-  final bool? openedRight;
+  /// 默认打开，[left, rigth]
+  final List<bool>? opened;
 
-  /// 默认打开左侧
-  final bool? openedLeft;
+  /// 右侧滑动操作项面板
+  final TDSwipePanel? right;
 
-  /// 右侧滑动操作项
-  final List<TDSwipeOption>? right;
-
-  /// 右侧滑动操作项组件，优先级高于right
-  final Widget? rightWidget;
-
-  /// 右侧滑动操作项二次确认
-  final List<TDSwipeOption>? rightConfirm;
-
-  /// 右侧滑动操作项二次确认组件，优先级高于rightConfirm
-  final Widget? rightConfirmWidget;
-
-  /// 左侧滑动操作项
-  final List<TDSwipeOption>? left;
-
-  /// 左侧滑动操作项组件，优先级高于left
-  final Widget? leftWidget;
-
-  /// 左侧滑动操作项二次确认
-  final List<TDSwipeOption>? leftConfirm;
-
-  /// 左侧滑动操作项二次确认组件，优先级高于leftConfirm
-  final Widget? leftConfirmWidget;
+  /// 左侧滑动操作项面板
+  final TDSwipePanel? left;
 
   /// 滑动展开事件
-  final Function(SwipeDirection direction, bool open)? onChange;
+  final Function(TDSwipeDirection direction, bool open)? onChange;
 
-  /// [SlidableController] 自定义控制滑动窗口
+  /// 自定义控制滑动窗口
   final SlidableController? controller;
 
-  /// 同一组中只有一个[Slidable]打开
+  /// 同一组中只有一个被打开
   final Object? groupTag;
+
+  /// 滚动时，是否关闭滑动操作项面板
+  final bool? closeOnScroll;
+
+  /// 处理拖动开始行为的方式[GestureDetector.dragStartBehavior]
+  final DragStartBehavior? dragStartBehavior;
+
+  /// 可拖动的方向
+  final Axis? direction;
 
   @override
   _TDSwipeCellState createState() => _TDSwipeCellState();
 }
 
-class _TDSwipeCellState extends State<TDSwipeCell> {
+class _TDSwipeCellState extends State<TDSwipeCell> with TickerProviderStateMixin {
+  late final SlidableController controller;
+  TDSwipeDirection? openDirection;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = (widget.controller ?? SlidableController(this))
+      ..actionPaneType.addListener(_handleActionPanelTypeChanged);
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      if ((widget.opened?.length ?? 0) > 0 && widget.opened![0] == true) {
+        controller.openStartActionPane();
+      }
+      if ((widget.opened?.length ?? 0) > 1 && widget.opened![1] == true) {
+        controller.openEndActionPane();
+      }
+    });
+  }
+
+  @override
+  void didUpdateWidget(covariant TDSwipeCell oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.controller != widget.controller) {
+      controller.actionPaneType.removeListener(_handleActionPanelTypeChanged);
+
+      controller = (widget.controller ?? SlidableController(this))
+        ..actionPaneType.addListener(_handleActionPanelTypeChanged);
+    }
+  }
+
+  @override
+  void dispose() {
+    controller.actionPaneType.removeListener(_handleActionPanelTypeChanged);
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // final cells = widget.cells ?? (widget.cell == null ? [] : [widget.cell!]);
-    return Container();
+    return Slidable(
+      key: widget.slidableKey ?? UniqueKey(),
+      closeOnScroll: widget.closeOnScroll ?? true,
+      child: widget.cell,
+      controller: controller,
+      enabled: !(widget.disabled ?? false),
+      groupTag: widget.groupTag,
+      startActionPane: widget.left?.build(context),
+      endActionPane: widget.right?.build(context),
+      dragStartBehavior: widget.dragStartBehavior ?? DragStartBehavior.start,
+      direction: widget.direction ?? Axis.horizontal,
+    );
+  }
+
+  void _handleActionPanelTypeChanged() {
+    if (widget.onChange == null) {
+      return;
+    }
+    switch (controller.actionPaneType.value) {
+      case ActionPaneType.none:
+        widget.onChange!(openDirection!, false);
+        openDirection = null;
+        break;
+      case ActionPaneType.start:
+        openDirection = TDSwipeDirection.left;
+        widget.onChange!(openDirection!, true);
+        break;
+      case ActionPaneType.end:
+        openDirection = TDSwipeDirection.right;
+        widget.onChange!(openDirection!, true);
+        break;
+    }
   }
 }
