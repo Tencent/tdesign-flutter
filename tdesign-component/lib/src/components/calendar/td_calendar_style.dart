cd1ae3d65cfaf30b29970fb018a75181daa9e08a
hkaikai <617760820@qq.com>
2024-07-25 17:45:06 +0800

    TDDropdownMenu 新增tabBarAlign参数；修复TDSlidePopupRoute 多次重置无效问题；TDCalendar body逻辑开发

--- tdesign-component/lib/src/components/calendar/td_calendar_style.dart
+++ tdesign-component/lib/src/components/calendar/td_calendar_style.dart
@@ -4,17 +4,36 @@ import '../../../tdesign_flutter.dart';
 /// 日历组件样式
 class TDCalendarStyle {
   TDCalendarStyle({
+    this.titleStyle,
     this.weekdayStyle,
+    this.titleMaxLine,
+    this.closeColor,
   });
+
+  ///  [TDCalendar.title]的样式
+  TextStyle? titleStyle;
 
   /// 周 文字样式
   TextStyle? weekdayStyle;
 
+  /// [TDCalendar.title]的行数
+  int? titleMaxLine;
+
+  /// 关闭图标的颜色
+  Color? closeColor;
+
   /// 生成默认样式
   TDCalendarStyle.generateStyle(BuildContext context) {
+    titleStyle = TextStyle(
+      fontSize: TDTheme.of(context).fontTitleLarge?.size,
+      fontWeight: TDTheme.of(context).fontTitleLarge?.fontWeight,
+      color: TDTheme.of(context).fontGyColor1,
+    );
     weekdayStyle = TextStyle(
       fontSize: TDTheme.of(context).fontTitleSmall?.size,
       color: TDTheme.of(context).fontGyColor2,
     );
+    titleMaxLine = 1;
+    closeColor = titleStyle?.color;
   }
 }
