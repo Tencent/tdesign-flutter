87c325340a5d2cec9e295a0828603edf894c1660
hkaikai <617760820@qq.com>
2024-08-01 15:55:49 +0800

    文档完善

--- tdesign-component/demo_tool/all_build.sh
+++ tdesign-component/demo_tool/all_build.sh
@@ -33,7 +33,7 @@
 
 # 输入
 # calendar
-./bin/demo_tool generate --folder ../lib/src/components/calendar --name TDCalendar,TDCalendarStyle,TDate --folder-name calendar --output ../example/assets/api/ --only-api --get-comments
+./bin/demo_tool generate --folder ../lib/src/components/calendar --name TDCalendar,TDCalendarPopup,TDCalendarStyle --folder-name calendar --output ../example/assets/api/ --only-api
 # cascader
 ./bin/demo_tool generate --folder ../lib/src/components/cascader --name TDMultiCascader --folder-name cascader --output ../example/assets/api/ --only-api
 
