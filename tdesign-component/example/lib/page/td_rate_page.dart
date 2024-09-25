ef17cbce8b7d6904cd496c7594d36c1bc4ddff55
hkaikai <617760820@qq.com>
2024-09-25 11:24:30 +0800

    rate组件初始化

---
+++ tdesign-component/example/lib/page/td_rate_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:tdesign_flutter/tdesign_flutter.dart';
+
+import '../annotation/demo.dart';
+import '../base/example_widget.dart';
+
+///
+/// TDRadio演示
+///
+class TDRatePage extends StatefulWidget {
+  const TDRatePage({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return TDRatePageState();
+  }
+}
+
+class TDRatePageState extends State<TDRatePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ExamplePage(
+        title: tdTitle(),
+        desc: '用于对某行为/事物进行打分。',
+        exampleCodeGroup: 'rate',
+        backgroundColor: TDTheme.of(context).grayColor2,
+        children: [
+          ExampleModule(title: '组件类型', children: [
+            ExampleItem(desc: '实心评分', builder: _buildFilledRate),
+          ]),
+        ]);
+  }
+
+  @Demo(group: 'rate')
+  Widget _buildFilledRate(BuildContext context) {
+    return Container();
+  }
+}
