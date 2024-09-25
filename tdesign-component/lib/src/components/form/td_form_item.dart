8ff5ac839540ec072bac8b710c64bb63518c3b24
Simon_Wzy <1691663479@qq.com>
2024-09-25 14:38:49 +0800

    formitem1.0

--- tdesign-component/lib/src/components/form/td_form_item.dart
+++ tdesign-component/lib/src/components/form/td_form_item.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-class TDFormIems extends StatelessWidget {
-  const TDFormIems({
-    Key?key,
+class TDFormItem extends StatelessWidget {
+  const TDFormItem({
+    Key? key,
     this.label,
     this.name,
     this.arrow = false,
@@ -13,22 +13,22 @@ class TDFormIems extends StatelessWidget {
     this.requiredMark,
     this.rules,
     this.showErrowMessage,
-  }): super(key: key);
+  }) : super(key: key);
 
   /// 表单内容对齐方式：
   /// 左对齐、右对齐
   /// 可选项：left/right
   final String? contentAlign;
-  
+
   /// 是否显示右箭头
   final bool? arrow;
-  
+
   /// 表单说明内容 ？？？？
   final help;
-  
+
   /// 表格内标签 内容填充
   final String? label;
-  
+
   /// 表单字段标签对齐方式：
   /// 左对齐、右对齐、顶部对齐
   /// 默认使用 Form 的对齐方式，其优先级高于 Form.labelAlign
@@ -52,9 +52,42 @@ class TDFormIems extends StatelessWidget {
   /// 校验不通过时，是否显示错误提示信息
   /// 优先级高于 Form.showErrowMessage
   final bool? showErrowMessage;
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Row(
+      mainAxisAlignment: contentAlign == 'right'
+          ? MainAxisAlignment.end
+          : MainAxisAlignment.start,
+      children: [
+        // 标签
+        Container(
+          width: labelWidth != null ? double.parse(labelWidth!) : null,
+          alignment: labelAlign == 'right'
+              ? Alignment.centerRight
+              : labelAlign == 'top'
+                  ? Alignment.topLeft
+                  : Alignment.centerLeft,
+          child: Text(
+            label ?? '',
+            style: TextStyle(
+              fontWeight:
+                  requiredMark == true ? FontWeight.bold : FontWeight.normal,
+            ),
+          ),
+        ),
+        const SizedBox(width: 8), // 标签与输入框的间距
+        // 输入框
+        Expanded(
+          child: TextFormField(
+            decoration: InputDecoration(
+              hintText: help,
+              suffixIcon: arrow == true ? Icon(Icons.arrow_forward) : null,
+            ),
+            // 可添加更多属性，如 validator 等
+          ),
+        ),
+      ],
+    );
   }
 }
