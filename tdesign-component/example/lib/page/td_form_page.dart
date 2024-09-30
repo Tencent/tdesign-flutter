5a2ab2a07add342c964460e5dd7ce70be6e107ce
Simon_Wzy <1691663479@qq.com>
2024-09-30 19:37:10 +0800

    初步实现了计数器和半个日期选择器

--- tdesign-component/example/lib/page/td_form_page.dart
+++ tdesign-component/example/lib/page/td_form_page.dart
@@ -4,14 +4,30 @@ import 'package:tdesign_flutter/tdesign_flutter.dart';
 import '../../annotation/demo.dart';
 import '../../base/example_widget.dart';
 
-class TDFormPage extends StatelessWidget {
+class TDFormPage extends StatefulWidget {
   const TDFormPage({Key? key}) : super(key: key);
 
   @override
+  _TDFormPageState createState() => _TDFormPageState();
+}
+
+class _TDFormPageState extends State<TDFormPage> {
+
+  var controller = [];
+  String selected_1 = '';
+
+  @override
+  void initState() {
+    for (var i = 0; i < 28; i++) {
+      controller.add(TextEditingController());
+    }
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // debugPaintBaselinesEnabled = true;
     return ExamplePage(
-      title: tdTitle(context),
+      title: tdTitle(),
       exampleCodeGroup: 'form',
       backgroundColor: const Color(0xfff6f6f6),
       children: [
@@ -22,76 +38,104 @@ class TDFormPage extends StatelessWidget {
           ExampleItem(desc: '', builder: _buildUserNameItem),
           ExampleItem(desc: '', builder: _buildPassWordItem),
           ExampleItem(desc: '', builder: _buildhorizontalRadiosItem),
+          ExampleItem(desc: '', builder: _buildDateItem),
+          ExampleItem(desc: '', builder: _buildStepperItem),
         ]),
       ],
     );
   }
-}
 
-@Demo(group: 'form')
-Widget _buildUserNameItem(BuildContext buildContext) {
-  
-  return TDFormItem(label:'用户名',
-  name: 'name',
-  help:'请输入用户名');
-}
+  @Demo(group: 'form')
+  Widget _buildUserNameItem(BuildContext buildContext) {
+    return TDFormItem(
+      label: '用户名',
+      name: 'name',
+      help: '请输入用户名',
+      controller: controller[0],
+    );
+  }
 
-@Demo(group: 'form')
-Widget _buildPassWordItem(BuildContext buildContext) {
-
-  return TDFormItem(label:'密码',
+  @Demo(group: 'form')
+  Widget _buildPassWordItem(BuildContext buildContext) {
+    return TDFormItem(
+      label: '密码',
       name: 'password',
-      help:'请输入密码');
-}
+      help: '请输入密码',
+      controller: controller[1],
+    );
+  }
 
-@Demo(group: 'form')
-Widget _buildhorizontalRadiosItem(BuildContext buildContext) {
-
-  return TDFormItem(label:'性别',
+  @Demo(group: 'form')
+  Widget _buildhorizontalRadiosItem(BuildContext buildContext) {
+    return TDFormItem(
+      label: '性别',
       name: 'gender',
-      );
-}
+      /// 扩展一下数量和选项内容
+    );
+  }
 
-@Demo(group: 'switch')
-Widget _buildSwitchWithBase(BuildContext context) {
-  return _buildItem(
-    context,
-    const TDSwitch(),
-    title: '禁用态',
-  );
-}
+  @Demo(group: 'form')
+  Widget _buildDateItem(BuildContext buildContext){
+    return TDFormItem(
+      label: '生日',
+      name: 'date',
+      /// 引入需要的日期数据
+      select: selected_1,
+    );
+  }
 
-/// 每一项的封装
-@Demo(group: 'switch')
-Widget _buildItem(BuildContext context, Widget switchItem,
-    {String? title, String? desc}) {
-  final theme = TDTheme.of(context);
-  Widget current = Row(
-    children: [
-      Expanded(
-          child: TDText(
-            title ?? '',
-            textColor: theme.fontGyColor1,
-          )),
-      TDText(
-        desc ?? '',
-        textColor: theme.grayColor6,
-      ),
-      SizedBox(child: switchItem)
-    ],
-  );
-  current = Padding(
-    padding: const EdgeInsets.symmetric(vertical: 8),
-    child: SizedBox(
-      child: Container(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: current,
+  @Demo(group: 'form')
+  Widget _buildStepperItem(BuildContext buildContext){
+    return TDFormItem(
+      label: '年限',
+      name: 'age',
+      /// 为 TDStepper 预留其他设置
+    );
+  }
+
+  @Demo(group: 'switch')
+  Widget _buildSwitchWithBase(BuildContext context) {
+    return _buildItem(
+      context,
+      const TDSwitch(),
+      title: '禁用态',
+    );
+  }
+
+  /// 每一项的封装
+  @Demo(group: 'switch')
+  Widget _buildItem(BuildContext context, Widget switchItem,
+      {String? title, String? desc}) {
+    final theme = TDTheme.of(context);
+    Widget current = Row(
+      children: [
+        Expanded(
+            child: TDText(
+              title ?? '',
+              textColor: theme.fontGyColor1,
+            )),
+        TDText(
+          desc ?? '',
+          textColor: theme.grayColor6,
         ),
-        color: Colors.white,
+        SizedBox(child: switchItem)
+      ],
+    );
+    current = Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: SizedBox(
+        child: Container(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: current,
+          ),
+          color: Colors.white,
+        ),
+        height: 56,
       ),
-      height: 56,
-    ),
-  );
-  return Column(mainAxisSize: MainAxisSize.min, children: [current]);
+    );
+    return Column(mainAxisSize: MainAxisSize.min, children: [current]);
+  }
 }
+
+
