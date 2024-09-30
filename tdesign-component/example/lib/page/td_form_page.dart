eb280e58b57be69a7524642098f719a9351e3db8
Simon_Wzy <1691663479@qq.com>
2024-09-30 14:13:10 +0800

    初步实现了四个功能

--- tdesign-component/example/lib/page/td_form_page.dart
+++ tdesign-component/example/lib/page/td_form_page.dart
@@ -7,62 +7,91 @@ import '../../base/example_widget.dart';
 class TDFormPage extends StatelessWidget {
   const TDFormPage({Key? key}) : super(key: key);
 
-  final exampleTxt = '文本Text';
-
   @override
   Widget build(BuildContext context) {
     // debugPaintBaselinesEnabled = true;
     return ExamplePage(
-      padding: const EdgeInsets.all(8),
       title: tdTitle(context),
-      exampleCodeGroup: 'text',
+      exampleCodeGroup: 'form',
+      backgroundColor: const Color(0xfff6f6f6),
       children: [
-        ExampleModule(title: '使用示例', children: [
-          ExampleItem(desc: '自定义内部padding:', builder: _buildUserNameItem),
+        ExampleModule(title: '禁用态开关', children: [
+          ExampleItem(desc: '基础开关', builder: _buildSwitchWithBase),
+        ]),
+        ExampleModule(title: 'Form', children: [
+          ExampleItem(desc: '', builder: _buildUserNameItem),
+          ExampleItem(desc: '', builder: _buildPassWordItem),
+          ExampleItem(desc: '', builder: _buildhorizontalRadiosItem),
         ]),
       ],
     );
   }
 }
 
-
 @Demo(group: 'form')
 Widget _buildUserNameItem(BuildContext buildContext) {
   
-  return TDFormItem();
-
+  return TDFormItem(label:'用户名',
+  name: 'name',
+  help:'请输入用户名');
 }
 
-// /// 自定义控件，内部的context可拿到外部TDTextConfiguration的配置信息
-// class CustomPaddingText extends StatelessWidget {
-//   const CustomPaddingText({Key? key}) : super(key: key);
+@Demo(group: 'form')
+Widget _buildPassWordItem(BuildContext buildContext) {
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       mainAxisSize: MainAxisSize.min,
-//       children: [
-//         TDText(
-//           '中华人民共和国腾讯科技fgjpqy',
-//           forceVerticalCenter: true,
-//           backgroundColor: TDTheme.of(context).brandFocusColor,
-//         ),
-//         TDText(
-//           'English',
-//           font: TDTheme.of(context).fontHeadlineLarge,
-//           forceVerticalCenter: true,
-//           backgroundColor: TDTheme.of(context).brandFocusColor,
-//         ),
-//       ],
-//     );
-//   }
-// }
+  return TDFormItem(label:'密码',
+      name: 'password',
+      help:'请输入密码');
+}
 
-// /// 重写内部padding方法
-// class CustomTextPaddingConfig extends TDTextPaddingConfig {
-//   @override
-//   EdgeInsetsGeometry getPadding(String? data, double fontSize, double height) {
-//     var supperPadding = super.getPadding(data, fontSize, height);
-//     return EdgeInsets.only(left: 30, top: supperPadding.vertical.toDouble());
-//   }
-// }
+@Demo(group: 'form')
+Widget _buildhorizontalRadiosItem(BuildContext buildContext) {
+
+  return TDFormItem(label:'性别',
+      name: 'gender',
+      );
+}
+
+@Demo(group: 'switch')
+Widget _buildSwitchWithBase(BuildContext context) {
+  return _buildItem(
+    context,
+    const TDSwitch(),
+    title: '禁用态',
+  );
+}
+
+/// 每一项的封装
+@Demo(group: 'switch')
+Widget _buildItem(BuildContext context, Widget switchItem,
+    {String? title, String? desc}) {
+  final theme = TDTheme.of(context);
+  Widget current = Row(
+    children: [
+      Expanded(
+          child: TDText(
+            title ?? '',
+            textColor: theme.fontGyColor1,
+          )),
+      TDText(
+        desc ?? '',
+        textColor: theme.grayColor6,
+      ),
+      SizedBox(child: switchItem)
+    ],
+  );
+  current = Padding(
+    padding: const EdgeInsets.symmetric(vertical: 8),
+    child: SizedBox(
+      child: Container(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: current,
+        ),
+        color: Colors.white,
+      ),
+      height: 56,
+    ),
+  );
+  return Column(mainAxisSize: MainAxisSize.min, children: [current]);
+}
