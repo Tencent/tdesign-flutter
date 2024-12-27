e93700a034bcb92dfb5b9edcad305d5f30d21c72
hkaikai <617760820@qq.com>
2024-12-27 13:15:17 +0800

    新增itemBuilder参数，用于自定义item

--- tdesign-component/lib/src/components/picker/td_picker.dart
+++ tdesign-component/lib/src/components/picker/td_picker.dart
@@ -31,7 +31,8 @@ class TDPicker {
       bool isTimeUnit=true,
       Function(int wheelIndex, int index)? onSelectedItemChanged,
       int pickerItemCount = 5,
-      List<int> Function(DateTypeKey key, List<int> nums)? filterItems}) {
+      List<int> Function(DateTypeKey key, List<int> nums)? filterItems,
+      ItemBuilderType? itemBuilder}) {
     if (dateEnd == null || initialDate == null) {
       var now = DateTime.now();
       // 如果未指定结束时间，则取当前时间
@@ -70,7 +71,8 @@ class TDPicker {
               ),
               pickerHeight: pickerHeight,
               pickerItemCount: pickerItemCount,
-              onSelectedItemChanged:onSelectedItemChanged);
+              onSelectedItemChanged:onSelectedItemChanged,
+              itemBuilder: itemBuilder);
         });
   }
 
@@ -91,7 +93,8 @@ class TDPicker {
       TextStyle? rightTextStyle,
       Color? titleDividerColor,
       double? topPadding,
-      int pickerItemCount = 5}) {
+      int pickerItemCount = 5,
+      ItemBuilderType? itemBuilder}) {
     showModalBottomSheet(
         context: context,
         backgroundColor: Colors.transparent,
@@ -112,6 +115,7 @@ class TDPicker {
             pickerItemCount: pickerItemCount,
             titleDividerColor: titleDividerColor,
             topPadding: topPadding,
+            itemBuilder: itemBuilder
           );
         });
   }
