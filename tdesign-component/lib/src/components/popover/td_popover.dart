7a43df5143801c6e10fefce2c5804317e1b6f95f
ccXxx1aoBai/ <1426169428@qq.com>
2025-01-02 17:30:41 +0800

    组件开发完成

--- tdesign-component/lib/src/components/popover/td_popover.dart
+++ tdesign-component/lib/src/components/popover/td_popover.dart
@@ -1,26 +1,45 @@
-
 import 'package:flutter/material.dart';
 
-import 'td_popover_style.dart';
-import 'td_popover_widget.dart';
+import '../../../tdesign_flutter.dart';
 
 class TDPopover {
 
   static Future showPopover({
     required BuildContext context,
-    required String content,
-    double? offset = 10,
+    String? content,
+    Widget? contentWidget,
+    double offset = 4,
     TDPopoverTheme? theme,
+    bool closeOnClickOutside = true,
+    TDPopoverPlacement? placement,
+    bool? showArrow = true,
+    double arrowSize = 8,
+    EdgeInsetsGeometry? padding,
+    double? width,
+    double? height,
+    Color? overlayColor = Colors.transparent,
+    OnTap? onTap,
+    OnLongTap? onLongTap,
   }) {
     return showDialog(
-      barrierDismissible: true,
-      barrierColor: Colors.transparent,
+      barrierDismissible: closeOnClickOutside,
+      barrierColor: overlayColor,
+      useSafeArea: false,
       context: context,
       builder: (ctx) => TDPopoverWidget(
         context: context,
         content: content,
+        contentWidget: contentWidget,
         offset: offset,
         theme: theme,
+        placement: placement,
+        showArrow: showArrow,
+        arrowSize: arrowSize,
+        padding: padding,
+        width: width,
+        height: height,
+        onTap: onTap,
+        onLongTap: onLongTap,
       ),
     );
   }
