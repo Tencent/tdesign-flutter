0ce38334b86bd62363f3141efa5fea3545f86d21
卢国祥 <noob-coder@qq.com>
2025-06-23 14:40:31 +0800

    refactor(l10n):优化本地化文件结构和代码格式
    - 移除了不必要的注释和导入
    - 调整了代码缩进和格式
    - 删除了未使用的 intl 导包
    - 统一了字符串格式

--- tdesign-component/example/lib/l10n/app_localizations_en.dart
+++ tdesign-component/example/lib/l10n/app_localizations_en.dart
@@ -1,8 +1,4 @@
-// ignore: unused_import
-import 'package:intl/intl.dart' as intl;
 import 'app_localizations.dart';
-
-// ignore_for_file: type=lint
 
 /// The translations for English (`en`).
 class AppLocalizationsEn extends AppLocalizations {
--- tdesign-component/example/lib/l10n/app_localizations_zh.dart
+++ tdesign-component/example/lib/l10n/app_localizations_zh.dart
@@ -1,8 +1,4 @@
-// ignore: unused_import
-import 'package:intl/intl.dart' as intl;
 import 'app_localizations.dart';
-
-// ignore_for_file: type=lint
 
 /// The translations for Chinese (`zh`).
 class AppLocalizationsZh extends AppLocalizations {
