637181b2af7455aeb5e9b9a525778bb9e38b58ab
卢国祥 <noob-coder@qq.com>
2025-10-02 19:03:35 +0800

    refactor(result): 重构结果页面和组件实现

--- tdesign-component/lib/src/components/result/td_result.dart
+++ tdesign-component/lib/src/components/result/td_result.dart
@@ -4,14 +4,13 @@ import '../../../tdesign_flutter.dart';
 enum TDResultTheme { defaultTheme, success, warning, error }
 
 class TDResult extends StatelessWidget {
-  /// 构造函数，用于初始化Result组件
   const TDResult({
     Key? key,
     this.description,
     this.icon,
     this.titleStyle,
-    this.theme = TDResultTheme.defaultTheme, // 默认主题样式为defaultTheme
-    this.title = '', // 默认标题为空字符串
+    this.theme = TDResultTheme.defaultTheme,
+    this.title = '',
   }) : super(key: key);
 
   /// 描述文本，用于提供额外信息
@@ -23,24 +22,22 @@ class TDResult extends StatelessWidget {
   /// 自定义字体样式，用于设置标题文本的样式
   final TextStyle? titleStyle;
 
-  /// 主题样式，定义了结果组件的视觉风格
+  /// 主题样式，默认主题样式为defaultTheme
   final TDResultTheme theme;
 
-  /// 标题文本，显示结果的主要信息
+  /// 标题文本，显示结果的主要信息，默认标题为空字符串
   final String title;
 
   @override
   Widget build(BuildContext context) {
     // 根据主题获取默认的图标组件
-    Widget displayIcon = icon ?? _getDefaultIconByTheme(context, theme);
-    // 构建组件布局
+    var displayIcon = icon ?? _getDefaultIconByTheme(context, theme);
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
         Container(
           child: displayIcon,
         ),
-        // 如果标题不为空，则显示标题
         if (title.isNotEmpty)
           Padding(
               padding: const EdgeInsets.only(top: 17),
@@ -50,13 +47,14 @@ class TDResult extends StatelessWidget {
                 font: TDTheme.of(context).fontTitleExtraLarge,
                 style: titleStyle,
               )),
-        // 如果描述不为空，则显示描述
-        if (description?.isNotEmpty ?? false)
+        if (description != null && description!.isNotEmpty)
           Padding(
             padding: const EdgeInsets.only(top: 8),
-            child: TDText(description!,
-                textColor: TDTheme.of(context).textColorSecondary,
-                font: TDTheme.of(context).fontTitleSmall),
+            child: TDText(
+              description!,
+              textColor: TDTheme.of(context).textColorSecondary,
+              font: TDTheme.of(context).fontTitleSmall,
+            ),
           ),
       ],
     );
@@ -66,17 +64,29 @@ class TDResult extends StatelessWidget {
   Widget _getDefaultIconByTheme(BuildContext context, TDResultTheme theme) {
     switch (theme) {
       case TDResultTheme.success:
-        return Icon(TDIcons.check_circle,
-            color: TDTheme.of(context).successNormalColor, size: 70);
+        return Icon(
+          TDIcons.check_circle,
+          color: TDTheme.of(context).successNormalColor,
+          size: 70,
+        );
       case TDResultTheme.warning:
-        return Icon(TDIcons.error_circle,
-            color: TDTheme.of(context).warningNormalColor, size: 70);
+        return Icon(
+          TDIcons.error_circle,
+          color: TDTheme.of(context).warningNormalColor,
+          size: 70,
+        );
       case TDResultTheme.error:
-        return Icon(TDIcons.close_circle,
-            color: TDTheme.of(context).errorNormalColor, size: 70);
+        return Icon(
+          TDIcons.close_circle,
+          color: TDTheme.of(context).errorNormalColor,
+          size: 70,
+        );
       default:
-        return Icon(TDIcons.info_circle,
-            color: TDTheme.of(context).brandNormalColor, size: 70);
+        return Icon(
+          TDIcons.info_circle,
+          color: TDTheme.of(context).brandNormalColor,
+          size: 70,
+        );
     }
   }
 }
