44fb1326ec2a14d3312d2b3018c7c1c21569154e
卢国祥 <noob-coder@qq.com>
2025-10-04 17:40:59 +0800

    feat(td_font_page): 添加【延】字符测试示例

--- tdesign-component/example/lib/page/td_font_page.dart
+++ tdesign-component/example/lib/page/td_font_page.dart
@@ -33,6 +33,7 @@ class TDFontPage extends StatelessWidget {
                       child: TDText(
                         '@$key:${value.size.toInt()}px',
                         font: value,
+
                         /// link类型的示例添加下划线
                         style: TextStyle(
                             decoration: key.contains('Link')
@@ -53,33 +54,73 @@ class TDFontPage extends StatelessWidget {
         ],
         test: [
           ExampleItem(
-              ignoreCode: true,
-              builder: (context) {
-                return Column(
-                  spacing: 16,
-                  children: [
-                    TDText(
-                      '使用主题字体：fontBodySmall',
-                      font: TDTheme.of(context).fontBodySmall,
-                    ),
-                    TDText(
-                      '使用主题字体：fontBodyLarge',
-                      font: TDTheme.of(context).fontBodyLarge,
-                    ),
-                    TDText(
-                      '不使用数字字体：1234567890abcd',
-                      font: TDTheme.defaultData().fontTitleSmall,
-                      textColor: TDTheme.of(context).brandNormalColor,
-                    ),
-                    TDText(
-                      '使用数字字体：1234567890abcd',
-                      font: TDTheme.defaultData().fontTitleSmall,
-                      textColor: TDTheme.of(context).brandNormalColor,
-                      fontFamily: TDTheme.defaultData().numberFontFamily,
-                    )
-                  ],
-                );
-              })
+            desc: '字体测试',
+            ignoreCode: true,
+            builder: (context) {
+              return Column(
+                spacing: 16,
+                children: [
+                  TDText(
+                    '使用主题字体：fontBodySmall',
+                    font: TDTheme.of(context).fontBodySmall,
+                  ),
+                  TDText(
+                    '使用主题字体：fontBodyLarge',
+                    font: TDTheme.of(context).fontBodyLarge,
+                  ),
+                  TDText(
+                    '不使用数字字体：1234567890abcd',
+                    font: TDTheme.defaultData().fontTitleSmall,
+                    textColor: TDTheme.of(context).brandNormalColor,
+                  ),
+                  TDText(
+                    '使用数字字体：1234567890abcd',
+                    font: TDTheme.defaultData().fontTitleSmall,
+                    textColor: TDTheme.of(context).brandNormalColor,
+                    fontFamily: TDTheme.defaultData().numberFontFamily,
+                  )
+                ],
+              );
+            },
+          ),
+          ExampleItem(
+            desc: '字符测试 - 待修复',
+            ignoreCode: true,
+            builder: (context) {
+              return Column(
+                spacing: 16,
+                children: [
+                  TDText(
+                    '延14字号',
+                    style: const TextStyle(fontSize: 14),
+                    font: TDTheme.of(context).fontMarkLarge,
+                  ),
+                  TDText(
+                    '延15字号',
+                    style: const TextStyle(fontSize: 15),
+                    font: TDTheme.of(context).fontMarkLarge,
+                  ),
+                  TDText(
+                    '延16字号',
+                    style: const TextStyle(fontSize: 16),
+                    font: TDTheme.of(context).fontMarkLarge,
+                  ),
+                  TDText(
+                    '延17字号',
+                    style: const TextStyle(fontSize: 17),
+                    font: TDTheme.of(context).fontMarkLarge,
+                  ),
+                  TDText(
+                    '延18字号',
+                    style: const TextStyle(fontSize: 18),
+                    font: TDTheme.of(context).fontMarkLarge,
+                  ),
+                  const Text('延-系统字体16字号', style: TextStyle(fontSize: 18)),
+                  const Text('延-系统字体18字号', style: TextStyle(fontSize: 18))
+                ],
+              );
+            },
+          ),
         ]);
   }
 }
