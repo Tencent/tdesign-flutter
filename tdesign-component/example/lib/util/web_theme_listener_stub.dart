069e31cccbb921567a09ec6c72687426541778ca
zflyluo <zflyluo@tencent.com>
2025-11-14 10:52:54 +0800

    暗色模式联动功能区分平台引入

---
+++ tdesign-component/example/lib/util/web_theme_listener_stub.dart
@@ -0,0 +1,11 @@
+// 非 Web 平台的 stub 实现
+
+import 'package:flutter/material.dart';
+
+import '../provider/theme_mode_provider.dart';
+
+/// 非 Web 平台的 stub 实现（空操作）
+void setupThemeModeListener(ThemeModeProvider themeModeProvider) {
+  // 非 Web 平台不需要实现，空操作即可
+}
+
