66f42b4258248ddcd402f1f59280fca696534eae
阿菜 Cai <jimmyrss1102@gmail.com>
2026-07-24 03:30:51 +0800

    fix: limit exports to migrated components

--- tdesign-component/lib/tdesign_flutter.dart
+++ tdesign-component/lib/tdesign_flutter.dart
@@ -1,19 +1,10 @@
 // v1.0 Icon: TIcons 常量来自 tdesign_icons 资源包
 export 'package:tdesign_icons/tdesign_icons.dart' show TIcons;
 
-export 'src/components/action_sheet/t_action_sheet.dart';
-export 'src/components/action_sheet/t_action_sheet_theme_data.dart'
-    show TActionSheetThemeData;
-export 'src/components/avatar/t_avatar.dart';
-export 'src/components/avatar/t_avatar_theme_data.dart' show TAvatarThemeData;
-export 'src/components/avatar/t_avatar_types.dart';
-export 'src/components/backtop/t_backtop.dart';
-export 'src/components/backtop/t_backtop_theme_data.dart'
-    show TBackTopThemeData, TBackTopShape;
-export 'src/components/badge/t_badge.dart';
-export 'src/components/badge/t_badge_theme_data.dart' show TBadgeThemeData;
+// Migrated base components
 export 'src/components/button/t_button.dart';
-export 'src/components/button/t_button_theme_data.dart' show TButtonThemeData;
+export 'src/components/button/t_button_theme_data.dart'
+    show TButtonThemeData;
 export 'src/components/button/t_button_types.dart'
     show
         TButtonColorScheme,
@@ -21,180 +12,22 @@ export 'src/components/button/t_button_types.dart'
         TButtonShape,
         TButtonSize,
         TButtonVariant;
-export 'src/components/calendar/t_calendar.dart';
-export 'src/components/calendar/t_calendar_theme_data.dart'
-    show TCalendarThemeData, TCalendarVariant;
-export 'src/components/cascader/t_cascader.dart';
-export 'src/components/cascader/t_cascader_theme_data.dart'
-    show TCascaderThemeData, TCascaderVariant;
-export 'src/components/cell/t_cell.dart';
-export 'src/components/cell/t_cell_group.dart';
-export 'src/components/cell/t_cell_theme_data.dart'
-    show TCellThemeData, TCellGroupVariant;
-export 'src/components/checkbox/t_check_box.dart';
-export 'src/components/checkbox/t_check_box_group.dart';
-export 'src/components/checkbox/t_checkbox_theme_data.dart'
-    show TCheckboxThemeData, TCheckboxVariant;
-export 'src/components/collapse/t_collapse.dart';
-export 'src/components/collapse/t_collapse_panel.dart';
-export 'src/components/collapse/t_collapse_theme_data.dart'
-    show TCollapseThemeData;
-export 'src/components/collapse/t_collapse_types.dart';
-export 'src/components/date_time_picker/t_date_time_picker.dart';
-export 'src/components/dialog/t_dialog.dart';
-export 'src/components/dialog/t_dialog_theme_data.dart' show TDialogThemeData;
 export 'src/components/divider/t_divider.dart';
 export 'src/components/divider/t_divider_theme_data.dart'
     show TDividerThemeData;
-export 'src/components/drawer/t_drawer.dart';
-export 'src/components/drawer/t_drawer_theme_data.dart' show TDrawerThemeData;
-export 'src/components/drawer/t_drawer_widget.dart'
-    show TDrawerItem, TDrawerItemClickCallback;
-export 'src/components/dropdown_menu/t_dropdown_item.dart';
-export 'src/components/dropdown_menu/t_dropdown_menu.dart';
-export 'src/components/dropdown_menu/t_dropdown_theme_data.dart'
-    show TDropdownThemeData;
-export 'src/components/empty/t_empty.dart';
-export 'src/components/empty/t_empty_theme_data.dart' show TEmptyThemeData;
 export 'src/components/fab/t_fab.dart';
 export 'src/components/fab/t_fab_theme_data.dart' show TFabThemeData;
-export 'src/components/footer/t_footer.dart';
-export 'src/components/footer/t_footer_theme_data.dart' show TFooterThemeData;
-export 'src/components/form/t_form.dart';
-export 'src/components/form/t_form_item.dart';
-export 'src/components/form/t_form_theme_data.dart'
-    show TFormLayout, TFormThemeData;
 export 'src/components/icon/t_icon.dart';
 export 'src/components/icon/t_icon_theme_data.dart' show TIconThemeData;
-export 'src/components/image/t_image.dart';
-export 'src/components/image/t_image_theme_data.dart' show TImageThemeData;
-export 'src/components/image_viewer/t_image_viewer.dart';
-export 'src/components/image_viewer/t_image_viewer_theme_data.dart'
-    show TImageViewerThemeData;
-export 'src/components/indexes/t_indexes.dart';
-export 'src/components/indexes/t_indexes_theme_data.dart'
-    show TIndexesThemeData;
-export 'src/components/input/t_input.dart';
-export 'src/components/input/t_input_theme_data.dart' show TInputThemeData;
 export 'src/components/link/t_link.dart';
 export 'src/components/link/t_link_theme_data.dart' show TLinkThemeData;
 export 'src/components/link/t_link_types.dart'
     show TLinkColorScheme, TLinkSize, TLinkVariant;
-export 'src/components/loading/t_loading.dart';
-export 'src/components/loading/t_loading_controller.dart';
-export 'src/components/loading/t_loading_theme_data.dart'
-    show TLoadingThemeData;
-export 'src/components/message/t_message.dart';
-export 'src/components/message/t_message_theme_data.dart'
-    show TMessageThemeData, TMessageVariant;
-export 'src/components/navbar/t_nav_bar.dart';
-export 'src/components/navbar/t_nav_bar_theme_data.dart'
-    show TNavBarThemeData, TNavBarBorder;
-export 'src/components/notice_bar/t_notice_bar.dart';
-export 'src/components/notice_bar/t_notice_bar_theme_data.dart'
-    show TNoticeBarThemeData, TNoticeBarVariant;
-export 'src/components/picker/t_picker.dart';
-export 'src/components/picker/t_picker_theme_data.dart' show TPickerThemeData;
-export 'src/components/picker/t_picker_types.dart';
-export 'src/components/popover/t_popover.dart';
-export 'src/components/popover/t_popover_theme_data.dart'
-    show TPopoverThemeData, TPopoverColorScheme;
-export 'src/components/popover/t_popover_widget.dart';
-export 'src/components/popup/t_popup.dart'
-    show
-        TPopup,
-        TPopupHandle,
-        TPopupOptions,
-        TPopupPlacement,
-        TPopupTrigger,
-        TPopupInset,
-        TPopupTopInset,
-        TPopupBottomInset,
-        TPopupLeftInset,
-        TPopupRightInset,
-        TPopupHeaderBuilder,
-        TPopupSlotBuilder,
-        TPopupVisibleChangeCallback;
-export 'src/components/popup/t_popup_theme_data.dart' show TPopupThemeData;
-export 'src/components/progress/t_progress.dart';
-export 'src/components/progress/t_progress_theme_data.dart'
-    show TProgressThemeData;
-export 'src/components/radio/t_radio.dart';
-export 'src/components/radio/t_radio_theme_data.dart' show TRadioThemeData;
-export 'src/components/rate/t_rate.dart';
-export 'src/components/rate/t_rate_theme_data.dart' show TRateThemeData;
-export 'src/components/refresh/t_refresh_header.dart';
-export 'src/components/refresh/t_refresh_theme_data.dart'
-    show TRefreshThemeData;
-export 'src/components/result/t_result.dart';
-export 'src/components/result/t_result_theme_data.dart' show TResultThemeData;
-export 'src/components/search/t_search_bar.dart';
-export 'src/components/search/t_search_bar_theme_data.dart'
-    show TSearchBarThemeData, TSearchBarVariant, TSearchBarAlignment;
-export 'src/components/sidebar/t_sidebar.dart';
-export 'src/components/sidebar/t_sidebar_item.dart';
-export 'src/components/sidebar/t_sidebar_theme_data.dart'
-    show TSideBarThemeData, TSideBarVariant;
-export 'src/components/skeleton/t_skeleton.dart';
-export 'src/components/skeleton/t_skeleton_rowcol.dart';
-export 'src/components/skeleton/t_skeleton_theme_data.dart'
-    show TSkeletonThemeData;
-export 'src/components/slider/t_slider.dart';
-export 'src/components/slider/t_slider_theme.dart';
-export 'src/components/stepper/t_stepper.dart';
-export 'src/components/stepper/t_stepper_theme_data.dart'
-    show TStepperThemeData;
-export 'src/components/steps/t_steps.dart';
-export 'src/components/steps/t_steps_theme_data.dart' show TStepsThemeData;
-export 'src/components/swipe_cell/t_swipe_cell.dart';
-export 'src/components/swipe_cell/t_swipe_cell_action.dart';
-export 'src/components/swipe_cell/t_swipe_cell_inherited.dart';
-export 'src/components/swipe_cell/t_swipe_cell_panel.dart';
-export 'src/components/swipe_cell/t_swipe_cell_theme_data.dart'
-    show TSwipeCellThemeData;
-export 'src/components/swiper/t_swiper.dart';
-export 'src/components/swiper/t_swiper_theme_data.dart'
-    show TSwiperThemeData;
-export 'src/components/swiper/t_swiper_types.dart';
-export 'src/components/switch/t_switch.dart';
-export 'src/components/switch/t_switch_theme_data.dart' show TSwitchThemeData;
-export 'src/components/switch/t_switch_types.dart'
-    show TSwitchSize, TSwitchVariant;
-export 'src/components/tabbar/t_tab_bar.dart';
-export 'src/components/tabbar/t_tab_bar_theme_data.dart' show TTabBarThemeData;
-export 'src/components/table/t_table.dart';
-export 'src/components/table/t_table_col.dart';
-export 'src/components/table/t_table_theme_data.dart' show TTableThemeData;
-export 'src/components/table/t_table_types.dart';
-export 'src/components/tabs/t_tab.dart';
-export 'src/components/tabs/t_tab_bar.dart' show TTabsBar, TTabsBarIndicator;
-export 'src/components/tabs/t_tab_bar_theme_data.dart'
-    show TTabsBarThemeData, TTabsBarVariant;
-export 'src/components/tabs/t_tab_bar_view.dart';
-export 'src/components/tag/t_select_tag.dart';
-export 'src/components/tag/t_tag.dart';
-export 'src/components/tag/t_tag_theme_data.dart' show TTagThemeData;
-export 'src/components/tag/t_tag_types.dart'
-    show TTagColorScheme, TTagShape, TTagSize;
 export 'src/components/text/t_font_loader.dart';
 export 'src/components/text/t_text.dart';
 export 'src/components/text/t_text_theme_data.dart' show TTextThemeData;
-export 'src/components/textarea/t_textarea.dart';
-export 'src/components/time_counter/t_time_counter.dart';
-export 'src/components/time_counter/t_time_counter_controller.dart'
-    show TTimeCounterController;
-export 'src/components/time_counter/t_time_counter_theme_data.dart'
-    show TTimeCounterThemeData;
-export 'src/components/time_counter/t_time_counter_types.dart'
-    show TTimeCounterDirection, TTimeCounterSize, TTimeCounterVariant;
-export 'src/components/toast/t_toast.dart';
-export 'src/components/toast/t_toast_theme_data.dart' show TToastThemeData;
-export 'src/components/tree/t_tree_select.dart';
-export 'src/components/tree/t_tree_select_theme_data.dart'
-    show TTreeSelectThemeData;
-export 'src/components/upload/t_upload.dart';
-export 'src/components/upload/t_upload_theme_data.dart'
-    show TUploadThemeData, TUploadVariant;
+
+// Theme foundation
 export 'src/theme/basic.dart';
 export 'src/theme/resource_delegate.dart';
 export 'src/theme/t_colors.dart';
@@ -204,5 +37,5 @@ export 'src/theme/t_radius.dart';
 export 'src/theme/t_shadows.dart';
 export 'src/theme/t_spacers.dart';
 export 'src/theme/t_theme.dart';
+
 export 'src/util/platform_util.dart';
-export 'src/util/t_toolbar_pressable.dart';
