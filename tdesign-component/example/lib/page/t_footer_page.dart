137cd3c793bb8b89504d19a29bac07efdb039025
阿菜 Cai <jimmyrss1102@gmail.com>
2026-09-09 15:54:13 +0800

    breaking(TFooter): consolidate content composition API (#1088)

--- tdesign-component/example/lib/page/t_footer_page.dart
+++ tdesign-component/example/lib/page/t_footer_page.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:tdesign_flutter/tdesign_flutter.dart';
-import '../../base/example_widget.dart';
 import '../annotation/example_code.dart';
+import '../base/example_widget.dart';
 
 class TFooterPage extends StatelessWidget {
   const TFooterPage({Key? key}) : super(key: key);
@@ -13,75 +12,60 @@ class TFooterPage extends StatelessWidget {
       title: tTitle(context),
       desc: '用于展示App的版权声明、联系信息、重要页面链接和其他相关内容等信息。',
       exampleCodeGroup: 'footer',
-      children: [
+      children: const [
         ExampleModule(
-          title: '组件类型',
+          title: '类型',
           children: [
             ExampleItem(desc: '基础页脚', builder: _buildFooter),
             ExampleItem(desc: '基础加链接页脚', builder: _buildSingleLinkFooter),
-            ExampleItem(desc: '', builder: _buildLinksFooter),
             ExampleItem(desc: '品牌页脚', builder: _buildBrandFooter),
           ],
         ),
       ],
     );
   }
+}
 
-  @ExampleCode(group: 'footer')
-  Widget _buildFooter(BuildContext context) {
-    return const TFooter(
-      TFooterVariant.text,
-      text: 'Copyright © 2019-2023 TDesign.All Rights Reserved.',
-    );
-  }
+@ExampleCode(group: 'footer')
+Widget _buildFooter(BuildContext context) {
+  return const TFooter(text: 'Copyright © 2021-2031 TD.All Rights Reserved.');
+}
 
-  @ExampleCode(group: 'footer')
-  Widget _buildSingleLinkFooter(BuildContext context) {
-    return TFooter(
-      TFooterVariant.link,
-      links: [
-        TLink(
-          child: const Text('底部链接'),
-          colorScheme: TLinkColorScheme.primary,
-          suffixIcon: const Icon(TIcons.jump),
-          onPressed: () {
-            print('点击了链接');
-          },
-        ),
-      ],
-      text: 'Copyright © 2019-2023 TDesign.All Rights Reserved.',
-    );
-  }
+@ExampleCode(group: 'footer')
+Widget _buildSingleLinkFooter(BuildContext context) {
+  TLink link(String text) => TLink(
+    child: Text(text),
+    colorScheme: TLinkColorScheme.primary,
+    onPressed: () {},
+  );
+  return Column(
+    children: [
+      TFooter(
+        links: [link('底部链接')],
+        text: 'Copyright © 2021-2031 TD.All Rights Reserved.',
+      ),
+      const SizedBox(height: 24),
+      TFooter(
+        links: [link('底部链接'), link('底部链接')],
+        text: 'Copyright © 2021-2031 TD.All Rights Reserved.',
+      ),
+    ],
+  );
+}
 
-  @ExampleCode(group: 'footer')
-  Widget _buildLinksFooter(BuildContext context) {
-    return TFooter(
-      TFooterVariant.link,
-      links: [
-        TLink(
-          child: const Text('底部链接1'),
-          colorScheme: TLinkColorScheme.primary,
-          onPressed: () {
-            print('点击了链接1');
-          },
-        ),
-        TLink(
-          child: const Text('底部链接2'),
-          colorScheme: TLinkColorScheme.primary,
-          onPressed: () {
-            print('点击了链接2');
-          },
-        ),
-      ],
-      text: 'Copyright © 2019-2023 TDesign.All Rights Reserved.',
-    );
-  }
-
-  @ExampleCode(group: 'footer')
-  Widget _buildBrandFooter(BuildContext context) {
-    return const TFooter(
-      TFooterVariant.brand,
-      logo: 'assets/img/t_brand.png',
-    );
-  }
+@ExampleCode(group: 'footer')
+Widget _buildBrandFooter(BuildContext context) {
+  const logo = TImage(
+    src: 'assets/img/t_brand.png',
+    width: 104,
+    height: 24,
+    fit: BoxFit.contain,
+  );
+  return const Column(
+    children: [
+      TFooter(logo: logo),
+      SizedBox(height: 24),
+      TFooter(logo: logo),
+    ],
+  );
 }
